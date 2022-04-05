9d202a494eb6996d5a06cfb3d8892ea1e9aaaa73
viraj21197 <karambelkarvraj21197@gmail.com>
2022-04-05 16:08:25 -0700

    add summer pipeline

--- winterdrp/pipelines/summer/summer_pipeline.py
+++ winterdrp/pipelines/summer/summer_pipeline.py
@@ -1,24 +1,36 @@
 import os
 import astropy.io.fits
 import numpy as np
+from astropy.coordinates import SkyCoord
+import astropy.units as u
 from astropy.io.fits import HDUList
 from winterdrp.pipelines.base_pipeline import Pipeline
 
 from winterdrp.processors.bias import BiasCalibrator
 from winterdrp.processors.flat import FlatCalibrator
+from winterdrp.processors.mask import MaskPixels
 from winterdrp.processors.utils import ImageSaver
-from winterdrp.processors.astromatic import Sextractor
-
+from winterdrp.processors.autoastrometry import AutoAstrometry
+from winterdrp.processors.astromatic import Sextractor, Scamp, Swarp
+from winterdrp.catalog import Gaia2Mass
+from winterdrp.pipelines.summer.summer_files import summer_mask_path, summer_weight_path, sextractor_astrometry_config, scamp_path, \
+    swarp_path
 from winterdrp.pipelines.summer.calibration import select_bias, select_flats_archival
 
 summer_flats_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+summer_gain = 1.0
+
+def summer_astrometric_catalog_generator(
+        header: astropy.io.fits.Header
+):
+    return Gaia2Mass(min_mag=10, max_mag=20, search_radius_arcmin=30)
 
 
 class SummerPipeline(Pipeline):
 
     name = "summer"
 
-    astrometry_cal = ("GAIA", 9., 13.)
+    astrometry_cal = ("GAIA", 10., 20.)
     photometry_cal = {
         "J": ()
     }
@@ -35,14 +47,42 @@ class SummerPipeline(Pipeline):
 
     batch_split_keys = ["RAWIMAGEPATH"]
 
+    #pipeline_configurations = {
+    #    None: [
+    #        (BiasCalibrator, select_bias),
+    #        (FlatCalibrator, select_flats_archival),
+    #        (ImageSaver, "preprocess"),
+    #        (Sextractor, "pass1"),
+    #        # "stack",
+    #        # "dither"
+    #    ]
+    #}
+
     pipeline_configurations = {
         None: [
-            (BiasCalibrator, select_bias),
-            (FlatCalibrator, select_flats_archival),
-            (ImageSaver, "preprocess"),
-            (Sextractor, "pass1"),
-            # "stack",
-            # "dither"
+            MaskPixels(mask_path=summer_mask_path),
+            BiasCalibrator(),
+            FlatCalibrator(),
+            #ImageSaver(output_dir_name="testa"),
+            AutoAstrometry(pa=0, inv=True, pixel_scale=0.466),
+            ImageSaver(output_dir_name="testb"),
+            Sextractor(
+                 output_sub_dir="postprocess",
+                 weight_image=summer_weight_path,
+                checkimage_name=None,
+                checkimage_type=None,
+                 **sextractor_astrometry_config
+             ),
+            #ImageSaver(output_dir_name="testc"),
+            Scamp(
+                 ref_catalog_generator=summer_astrometric_catalog_generator,
+                 scamp_config_path=scamp_path,
+             ),
+            #ImageSaver(output_dir_name="testd"),
+            Swarp(swarp_config_path=swarp_path,imgpixsize=2400),
+            #ImageSaver(output_dir_name="latest"),
+            #PhotCalibrator(ref_catalog_generator=summer_photometric_catalog_generator),
+            #PhotCalibrator(ref_catalog_generator=summer_backup_photometric_catalog_generator,redo=False),
         ]
     }
 
@@ -53,9 +93,33 @@ class SummerPipeline(Pipeline):
     ) -> [np.array, astropy.io.fits.Header]:
         header = img[0].header
         header["OBSCLASS"] = ["calibration", "science"][header["OBSTYPE"] == "SCIENCE"]
+        #print(header['OBSCLASS'])
+        header['UTCTIME'] = header['UTCSHUT']
+        header['TARGET'] = header['OBSTYPE'].lower()
+        #header['TARGET'] = header['FIELDID']
+        crd = SkyCoord(ra=img[0].header['RA'], dec=img[0].header['DEC'], unit=(u.deg, u.deg))
+        header['RA'] = crd.ra.deg
+        header['DEC'] = crd.dec.deg
+        header['CRVAL1'] = header['RA']
+        header['CRVAL2'] = header['DEC']
+        tel_crd = SkyCoord(ra=img[0].header['TELRA'], dec=img[0].header['TELDEC'], unit=(u.deg, u.deg))
+        header['TELRA'] = tel_crd.ra.deg
+        header['TELDEC'] = tel_crd.dec.deg
+        #filters = {'4': 'OPEN', '3': 'r', '1': 'u'}
+        header['BZERO'] = 0
+
+        #print(img[0].data.shape)
+        img[0].data = img[0].data * 1.0
+        #img[0].data[2048, :] = np.nan
+
+        if 'other' in header['FILTERID']:
+            header['FILTERID'] = 'r'
+
+        if "CALSTEPS" not in header.keys():
+            header["CALSTEPS"] = ""
         header["BASENAME"] = os.path.basename(path)
-        header["CALSTEPS"] = ""
-        header["UTCTIME"] = header["UTCISO"].replace(" ", "T")
+        header.append(('GAIN', summer_gain, 'Gain in electrons / ADU'), end=True)
+
         img[0].header = header
         return img[0].data, img[0].header
 
