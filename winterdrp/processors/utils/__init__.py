a8317f0f6447b6e85c5cb5379a139c11ac83b4f7
Robert Stein <robert.stein@desy.de>
2022-04-07 23:29:44 -0700

    New util processor for logging header values

--- winterdrp/processors/utils/__init__.py
+++ winterdrp/processors/utils/__init__.py
@@ -1,1 +1,2 @@
 from winterdrp.processors.utils.image_saver import ImageSaver
+from winterdrp.processors.utils.header_reader import HeaderReader
---
+++ winterdrp/processors/utils/header_reader.py
@@ -0,0 +1,35 @@
+import logging
+import astropy.io.fits
+import numpy as np
+from winterdrp.processors.base_processor import BaseProcessor
+from winterdrp.paths import base_name_key
+
+
+logger = logging.getLogger(__name__)
+
+
+class HeaderReader(BaseProcessor):
+
+    base_key = "header_reader"
+
+    def __init__(
+            self,
+            *keys,
+    ):
+        super().__init__()
+        self.keys = keys
+
+    def _apply_to_images(
+            self,
+            images: list[np.ndarray],
+            headers: list[astropy.io.fits.Header],
+    ) -> tuple[list[np.ndarray], list[astropy.io.fits.Header]]:
+
+        for header in headers:
+
+            msg = f"{header[base_name_key]} \n"
+            for key in self.keys:
+                msg += f"{key}: {header[key]} \n"
+            logger.info(msg)
+
+        return images, headers
