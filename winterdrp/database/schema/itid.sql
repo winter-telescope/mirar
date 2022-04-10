3652faf7585d0cf5d2d56c614bca4b56cccc2c3d
viraj21197 <karambelkarvraj21197@gmail.com>
2022-04-10 02:22:43 -0700

    Update itid.sql schema file

--- winterdrp/database/schema/itid.sql
+++ winterdrp/database/schema/itid.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS itid( itid INT PRIMARY KEY,  imgtype VARCHAR(20));
-INSERT INTO itid AS t1
+INSERT INTO itid (itid, imgtype)
 SELECT * FROM (SELECT 1,'SCIENCE' UNION ALL
                SELECT 2,'CAL' UNION ALL
                SELECT 3,'FOCUS' UNION ALL
                SELECT 4,'POINTING' UNION ALL
                SELECT 5,'OTHER') data
-               WHERE NOT EXISTS (SELECT NULL FROM itid AS t2);
+               WHERE NOT EXISTS (SELECT NULL FROM itid );
