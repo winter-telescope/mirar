1c6759bcd3ddc95b038f8e30b6bc3cfab879cdab
Robert Stein <rdstein@caltech.edu>
2022-06-24 15:33:50 -0700

    ImageSelector processor

--- winterdrp/processors/utils/image_selector.py
+++ winterdrp/processors/utils/image_selector.py
@@ -43,3 +43,64 @@ class ImageSelector(BaseProcessor):
     ) -> tuple[list[np.ndarray], list[astropy.io.fits.Header]]:
         return select_from_images(images, headers, target=self.target)
 
+
+def split_images_into_batches(
+        images: list[np.ndarray],
+        headers: list[astropy.io.fits.Header],
+        split_key: str | list[str]
+) -> list[list[list[np.ndarray], list[astropy.io.fits.Header]]]:
+
+    if isinstance(split_key, str):
+        split_key = [split_key]
+
+    groups = dict()
+
+    for i, header in enumerate(headers):
+        uid = []
+
+        for key in split_key:
+            uid.append(str(header[key]))
+
+        uid = "_".join(uid)
+
+        if uid not in groups.keys():
+            groups[uid] = [[images[i]], [header]]
+        else:
+            groups[uid][0] += [images[i]]
+            groups[uid][1] += [header]
+
+    return [x for x in groups.values()]
+
+
+class ImageBatcher(BaseProcessor):
+
+    base_key = "batch"
+
+    def __init__(
+            self,
+            split_key: str | list[str],
+            *args,
+            **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.split_key = split_key
+
+    def _apply_to_images(
+            self,
+            images: list[np.ndarray],
+            headers: list[astropy.io.fits.Header],
+    ) -> tuple[list[np.ndarray], list[astropy.io.fits.Header]]:
+        return images, headers
+
+    def update_batches(
+        self,
+        batches: list[list[list[np.ndarray], list[astropy.io.fits.header]]]
+    ) -> list[list[list[np.ndarray], list[astropy.io.fits.header]]]:
+
+        new_batches = []
+
+        for [images, headers] in batches:
+            new_batches += split_images_into_batches(images, headers, split_key=self.split_key)
+
+        return new_batches
+
