ff29599d2d50ee71dbdf2f0e4334c16bf1c1cb37
Robert Stein <rdstein@caltech.edu>
2022-06-24 15:00:04 -0700

    Batchify

--- winterdrp/processors/base_processor.py
+++ winterdrp/processors/base_processor.py
@@ -3,12 +3,14 @@ from abc import ABC
 
 import astropy.io.fits
 import numpy as np
-import pandas as pd
+import os
 import socket
 import getpass
 import datetime
 from winterdrp.io import save_to_path, open_fits
-from winterdrp.paths import raw_img_key, get_mask_path, latest_save_key, latest_mask_save_key
+from winterdrp.paths import cal_output_sub_dir, get_mask_path, latest_save_key, latest_mask_save_key, get_output_path,\
+    ProcessingError, base_name_key
+from winterdrp.errors import ErrorReport
 
 logger = logging.getLogger(__name__)
 
@@ -28,7 +30,6 @@ class BaseProcessor:
             *args,
             **kwargs
     ):
-        self.cache = dict()
 
         self.night = None
         self.night_sub_dir = None
@@ -45,13 +46,6 @@ class BaseProcessor:
             headers: list[astropy.io.fits.Header],
     ) -> tuple[list[np.ndarray], list[astropy.io.fits.Header]]:
         raise NotImplementedError
-
-    # def apply_previous(
-    #         self,
-    #         images: list[np.ndarray],
-    #         headers: list[astropy.io.fits.header],
-    # ) -> (list[np.ndarray], list[astropy.io.fits.header]):
-    #     raise NotImplementedError
 
     def set_preceding_steps(
             self,
@@ -80,12 +74,32 @@ class BaseProcessor:
 
     def apply(
             self,
-            images: list[np.ndarray],
-            headers: list[astropy.io.fits.header],
-    ) -> tuple[list[np.ndarray], list[astropy.io.fits.Header]]:
-        images, headers = self._apply_to_images(images, headers)
-        headers = self._update_processing_history(headers)
-        return images, headers
+            batches: list[list[list[np.ndarray], list[astropy.io.fits.header]]]
+    ) -> tuple[list[list[list[np.ndarray], list[astropy.io.fits.header]]], list]:
+
+        passed_batches = []
+        failures = []
+
+        for i, [images, headers] in enumerate(batches):
+
+            try:
+                images, headers = self._apply_to_images(images, headers)
+                headers = self._update_processing_history(headers)
+                passed_batches.append([images, headers])
+            except ProcessingError as e:
+                err = ErrorReport(e, self.__module__, images, headers)
+                logger.error(err.generate_log_message())
+                failures.append(err)
+
+        batches = self.update_batches(passed_batches)
+
+        return batches, failures
+
+    @staticmethod
+    def update_batches(
+        batches: list[list[list[np.ndarray], list[astropy.io.fits.header]]]
+    ) -> list[list[list[np.ndarray], list[astropy.io.fits.header]]]:
+        return batches
 
     def check_prerequisites(
             self,
@@ -98,16 +112,13 @@ class BaseProcessor:
     ) -> tuple[np.ndarray, astropy.io.fits]:
         return open_fits(path)
 
+    @staticmethod
     def save_fits(
-            self,
             data,
             header,
             path,
-            copy_to_cache: bool = False
     ):
         header[latest_save_key] = path
-        if copy_to_cache:
-            self.cache[path] = (data, header)
         logger.info(f"Saving to {path}")
         save_to_path(data, header, path)
 
@@ -123,81 +134,138 @@ class BaseProcessor:
         self.save_fits(mask, header, mask_path)
         return mask_path
 
-    def load_and_apply_previous(
-            self,
-            img_path: str
-    ) -> tuple[np.ndarray, astropy.io.fits.Header]:
-
-        img, header = self.open_fits(img_path)
-
-        # Iteratively apply corrections
-        for p in self.preceding_steps:
-            [img], [header] = p.apply([img], [header])
-
-        return img, header
-
 
 class ProcessorWithCache(BaseProcessor, ABC):
 
-    @staticmethod
-    def select_cache_images(
-            observing_log: pd.DataFrame
-    ) -> list:
+    def __init__(
+            self,
+            try_load_cache: bool = False,
+            write_to_cache: bool = True,
+            overwrite: bool = True,
+            cache_sub_dir: str = cal_output_sub_dir,
+            *args,
+            **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.try_load_cache = try_load_cache
+        self.write_to_cache = write_to_cache
+        self.overwrite = overwrite
+        self.cache_sub_dir = cache_sub_dir
+
+    def get_cache_path(
+            self,
+            images: list[np.ndarray],
+            headers: list[astropy.io.fits.Header],
+    ) -> str:
+
+        file_name = self.get_cache_file_name(images, headers)
+
+        output_path = get_output_path(
+            base_name=file_name,
+            dir_root=self.cache_sub_dir,
+            sub_dir=self.night_sub_dir
+        )
+
+        return output_path
+
+    def get_cache_file_name(
+            self,
+            images: list[np.ndarray],
+            headers: list[astropy.io.fits.Header],
+    ) -> str:
         raise NotImplementedError
 
-    def load_cache_file(
+    def get_cache_file(
             self,
-            path: str
-    ) -> (np.ndarray, astropy.io.fits.Header):
+            images: list[np.ndarray],
+            headers: list[astropy.io.fits.Header],
+    ) -> tuple[np.ndarray, astropy.io.fits.Header]:
 
-        if path in self.cache:
-            img, header = self.cache[path]
+        path = self.get_cache_path(images, headers)
+
+        exists = os.path.exists(path)
+
+        if np.logical_and(self.try_load_cache, exists):
+            return self.open_fits(path)
+
         else:
-            img, header = self.open_fits(path)
-            self.cache[path] = (img, header)
-        return img, header
 
-    def make_cache(
+            image, header = self.make_image(images, headers)
+
+            if self.write_to_cache:
+                if np.sum([not exists, self.overwrite]) > 0:
+                    self.save_fits(image, header, path)
+
+        return image, header
+
+    def make_image(
             self,
-            observing_log: pd.DataFrame,
-    ):
-
-        img_path_list = self.select_cache_images(observing_log)
-
-        if len(img_path_list) > 0:
-            self.make_cache_files(
-                img_path_list,
-            )
-
-    def make_cache_files(
-            self,
-            image_paths: list[str],
-    ):
+            images: list[np.ndarray],
+            headers: list[astropy.io.fits.Header],
+    ) -> tuple[np.ndarray, astropy.io.fits.Header]:
         raise NotImplementedError
 
-    def subselect_log(
-            self,
-            observing_log: pd.DataFrame,
-            key: str
-    ) -> pd.DataFrame:
-        mask = np.logical_and(
-            observing_log["TARGET"] == key,
-            observing_log["NIGHT"] == self.night
-        )
-
-        return observing_log[mask]
-
-    def select_from_log(
-            self,
-            observing_log: pd.DataFrame,
-            key: str
-    ) -> [str]:
-
-        obs = self.subselect_log(
-            observing_log=observing_log,
-            key=key
-        )
-
-        logger.debug(f"Found {len(obs)} entries with key '{key}' for night '{self.night}'")
-
-        return list(obs[raw_img_key])
+# class ProcessorWithCache(BaseProcessor, ABC):
+#
+#     @staticmethod
+#     def select_cache_images(
+#             observing_log: pd.DataFrame
+#     ) -> list:
+#         raise NotImplementedError
+#
+#     def load_cache_file(
+#             self,
+#             path: str
+#     ) -> (np.ndarray, astropy.io.fits.Header):
+#
+#         if path in self.cache:
+#             img, header = self.cache[path]
+#         else:
+#             img, header = self.open_fits(path)
+#             self.cache[path] = (img, header)
+#         return img, header
+#
+#     def make_cache(
+#             self,
+#             observing_log: pd.DataFrame,
+#     ):
+#
+#         img_path_list = self.select_cache_images(observing_log)
+#
+#         if len(img_path_list) > 0:
+#             self.make_cache_files(
+#                 img_path_list,
+#             )
+#
+#     def make_cache_files(
+#             self,
+#             image_paths: list[str],
+#     ):
+#         raise NotImplementedError
+#
+#     def subselect_log(
+#             self,
+#             observing_log: pd.DataFrame,
+#             key: str
+#     ) -> pd.DataFrame:
+#         mask = np.logical_and(
+#             observing_log["TARGET"] == key,
+#             observing_log["NIGHT"] == self.night
+#         )
+#
+#         return observing_log[mask]
+#
+#     def select_from_log(
+#             self,
+#             observing_log: pd.DataFrame,
+#             key: str
+#     ) -> [str]:
+#
+#         obs = self.subselect_log(
+#             observing_log=observing_log,
+#             key=key
+#         )
+#
+#         logger.debug(f"Found {len(obs)} entries with key '{key}' for night '{self.night}'")
+#
+#         return list(obs[raw_img_key])
