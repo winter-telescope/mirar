b202d414138bcc7b4e2af814da3c7a05f5741842
viraj21197 <karambelkarvraj21197@gmail.com>
2022-06-30 15:22:19 -0700

    Add candidate filtering

--- winterdrp/pipelines/wirc_imsub/wirc_imsub_pipeline.py
+++ winterdrp/pipelines/wirc_imsub/wirc_imsub_pipeline.py
@@ -138,27 +138,3 @@ class WircImsubPipeline(Pipeline):
                 header['CALSTEPS'] = ''
         return data, header
 
-    '''
-            MaskPixels(mask_path=wirc_mask_path),
-            DarkCalibrator(),
-            SkyFlatCalibrator(),
-            NightSkyMedianCalibrator(),
-            AutoAstrometry(catalog="tmc"),
-            Sextractor(
-                output_sub_dir="postprocess",
-                **sextractor_astrometry_config
-            ),
-            Scamp(
-                ref_catalog_generator=wirc_astrometric_catalog_generator,
-                scamp_config_path=scamp_fp_path,
-            ),
-            Swarp(swarp_config_path=swarp_sp_path),
-            Sextractor(
-                output_sub_dir="final_sextractor",
-                **sextractor_astrometry_config
-            ),
-            ImageSaver(output_dir_name="final"),
-            PhotCalibrator(ref_catalog_generator=wirc_photometric_catalog_generator),
-        ]
-    }
-    '''
