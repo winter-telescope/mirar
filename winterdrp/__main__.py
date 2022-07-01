c49184e29ae1ff52785cdf3607a1539d33e4a56c
Robert Stein <rdstein@caltech.edu>
2022-07-01 12:59:23 -0700

    Spin off logs as a Processor

--- winterdrp/__main__.py
+++ winterdrp/__main__.py
@@ -11,61 +11,6 @@ logger = logging.getLogger(__name__)
 parser = argparse.ArgumentParser(
     description="winterdrp: An automated image reduction pipeline, developed for WINTER"
 )
-# parser.add_argument(
-#     '-d',
-#     "--datadir",
-#     help='Data directory : Specify the name of the directory with raw files'
-# )
-# parser.add_argument(
-#     '--o',
-#     default=None,
-#     help='Object name : Give the name of the object to be reduced.'
-# )
-# parser.add_argument(
-#     '--f',
-#     default=None,
-#     help='Filter name: Specify filter to be reduced (options are J, H and Ks).'
-# )
-# parser.add_argument(
-#     '--g',
-#     help='Extended: Look for frames with extended emission, and do not use them for sky background.',
-#     action='store_true',
-#     default=False
-# )
-# parser.add_argument(
-#     '--dk',
-#     default=None,
-#     help='Dark file list : Give a list of files with darks for the images to be reduced.'
-# )
-# parser.add_argument(
-#     '--so',
-#     default=None,
-#     help='Source file list : Give list of files with source images to be reduced.'
-#          ' Must be combined with corresponding sky file list.'
-# )
-# parser.add_argument(
-#     '--sk',
-#     default=None,
-#     help='Sky file list: Give list of files corresponding to the source images. '
-#          'Must be combined with corresponding source file list.'
-# )
-# parser.add_argument(
-#     '--m',
-#     default=None,
-#     help='Mode specific execution: Run only specific steps of the reduction pipeline. '
-#          'Options are fp (FIRST-PASS), sp (SECOND-PASS), as (ASTROMETRY AND STACKING), '
-#          'pc (PHOTOMETRIC CALIBRATION). Join multiple modes with with spaceless commas, '
-#          'e.g "--m fp,sp". Note that running each step expects that the previous step '
-#          'has already been run, and expects the associated data products in the '
-#          'reduction directory (except first-pass, which is the first step anyways)'
-# )
-# parser.add_argument(
-#     '--c',
-#     help='Add this option along with -m if you want a specific step '
-#          'and all subsequent steps to be executed',
-#     action='store_true',
-#     default=False
-# )
 parser.add_argument(
     "-n",
     "--night",
@@ -101,12 +46,6 @@ parser.add_argument(
     help="Only process a specific image batch"
 )
 parser.add_argument(
-    '--skipcache',
-    help='Skip building cache',
-    action='store_true',
-    default=False
-)
-parser.add_argument(
     '--download',
     help='Download images from server',
     action='store_true',
@@ -115,12 +54,6 @@ parser.add_argument(
 parser.add_argument(
     '--db',
     help='Set up database',
-    action='store_true',
-    default=False
-)
-parser.add_argument(
-    '--remake_logs',
-    help='Rebuild log even if it exists',
     action='store_true',
     default=False
 )
@@ -147,29 +80,6 @@ handler.setFormatter(formatter)
 log.addHandler(handler)
 log.setLevel(args.level)
 
-# Parse running modes
-
-# if args.datadir is None:
-#     err = "No data directory specified. Please rerun with '--da /path/to/data'"
-#     logger.error(err)
-#     raise Exception(err)
-
-# if args.m is None:
-#     modes = wp.all_modes
-# else:
-#     modes = [x for x in wp.all_modes if x in args.m.split(",")]
-#
-# if args.c:
-#
-#     if len(modes) > 1:
-#         warning = f"Running with '-c' option (continue), so running all steps after '{modes[0]}'. " \
-#                   f"However, multiple modes were specified ({modes}), so the command is ambiguous."
-#         logger.warning(warning)
-#
-#     modes = wp.all_modes[wp.all_modes.index(modes[0]):]
-
-# logger.info(f"Running modes selected: {modes}")
-
 if args.download:
 
     Pipeline.pipelines[args.pipeline.lower()].download_raw_images_for_night(
@@ -178,26 +88,12 @@ if args.download:
 
     logger.info("Download complete")
 
-# if args.db:
-#     set_up_db(data_dir=raw_img_dir(os.path.join(args.pipeline.lower(), args.night)))
-
 pipe = get_pipeline(
     args.pipeline,
     configuration=args.config,
     night=args.night,
-    skip_build_cache=args.skipcache,
-    remake_logs=args.remake_logs
 )
 
-# pipe.make_calibration_files(sub_dir=args.subdir)
-# image_batches = pipe.split_raw_images_into_batches(
-#     select_batch=args.batch
-# )
-
 pipe.reduce_images([[[], []]])
-
-# for image_batch in image_batches:
-#     images, headers = pipe.open_raw_image_batch(image_batch)
-#     pipe.reduce_images(images, headers)
 
 logger.info('End of winterdrp execution')
