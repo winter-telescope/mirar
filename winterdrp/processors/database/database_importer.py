bceb036683eff2b89394690c95a2b51006852d0e
Robert Stein <rdstein@caltech.edu>
2022-07-11 17:18:50 -0700

    Add more flexible options for Database importing

--- winterdrp/processors/database/database_importer.py
+++ winterdrp/processors/database/database_importer.py
@@ -1,12 +1,15 @@
 import astropy.io.fits
+from astropy.io.fits import Header
 import numpy as np
 from abc import ABC
+from collections.abc import Callable
 
 import pandas as pd
 
 from winterdrp.processors.base_processor import BaseImageProcessor, BaseDataframeProcessor
 import logging
-from winterdrp.processors.database.base_database_processor import BaseDatabaseProcessor
+from winterdrp.processors.database.base_database_processor import BaseDatabaseProcessor, DataBaseError
+from winterdrp.processors.database.postgres import import_from_db
 
 logger = logging.getLogger(__name__)
 
@@ -15,19 +18,31 @@ class BaseDatabaseImporter(BaseDatabaseProcessor, ABC):
 
     base_key = "dbimporter"
 
+
+def update_header_with_single_match(
+        header: Header,
+        res: list[dict]
+) -> Header:
+    assert len(res) == 1
+
+    for key, value in res[0]:
+        header[key] = value
+
+    return header
+
+
+class BaseImageDatabaseImporter(BaseDatabaseImporter, BaseImageProcessor):
+
     def __init__(
             self,
-            result_field_name: str,
-            import_from_db: callable,
+            db_output_columns: str | list[str],
+            update_header: Callable[[Header, list[dict]], Header] = update_header_with_single_match,
             *args,
             **kwargs
     ):
         super().__init__(*args, **kwargs)
-        self.result_field_name = result_field_name
-        self.import_from_db = import_from_db
-
-
-class DatabaseImageImporter(BaseDatabaseImporter, BaseImageProcessor):
+        self.update_header = update_header
+        self.db_output_columns = db_output_columns
 
     def _apply_to_images(
             self,
@@ -36,37 +51,43 @@ class DatabaseImageImporter(BaseDatabaseImporter, BaseImageProcessor):
     ) -> tuple[list[np.ndarray], list[astropy.io.fits.Header]]:
 
         for i, header in enumerate(headers):
-            header = self.import_from_db(
-                header,
+
+            query_columns, accepted_values = self.get_constraints(header)
+
+            res = import_from_db(
                 db_name=self.db_name,
                 db_table=self.db_table,
+                db_query_columns=query_columns,
+                db_accepted_values=accepted_values,
+                db_output_columns=self.db_output_columns,
                 db_user=self.db_user,
                 password=self.db_password
             )
-            headers[i] = header
+
+            headers[i] = self.update_header(header, res)
 
         return images, headers
 
+    def get_constraints(self, header):
+        raise NotImplementedError
+
+
+class CrossmatchDatabaseWithHeader(BaseImageDatabaseImporter):
+
+    def __init__(
+            self,
+            db_query_columns: str | list[str],
+            *args,
+            **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.db_query_columns = db_query_columns
+
+    def get_constraints(self, header) -> list[str]:
+        accepted_values = [header[x.upper()] for x in self.db_query_columns]
+        return accepted_values
+
 
 class DatabaseDataframeImporter(BaseDatabaseImporter, BaseDataframeProcessor):
-
-    def _apply_to_candidates(
-            self,
-            candidate_table: pd.DataFrame
-    ) -> pd.DataFrame:
-
-        new_table = pd.DataFrame()
-
-        for index, candidate_row in candidate_table.iterrows():
-            new_row = self.import_from_db(
-                candidate_row,
-                db_name=self.db_name,
-                db_table=self.db_table,
-                db_user=self.db_user,
-                password=self.db_password
-            )
-
-            new_table.append(new_row, ignore_index=True)
-
-        return new_table
+    pass
 
