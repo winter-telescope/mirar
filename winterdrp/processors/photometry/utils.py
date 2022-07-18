a373aa40a29d04f7723622f4b21975442b26a746
viraj21197 <karambelkarvraj21197@gmail.com>
2022-07-17 19:31:03 -0700

    fix bug in make_cutouts

--- winterdrp/processors/photometry/utils.py
+++ winterdrp/processors/photometry/utils.py
@@ -1,5 +1,8 @@
 from astropy.io import fits
 import numpy as np
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def make_cutouts(
@@ -11,7 +14,21 @@ def make_cutouts(
     y_image_size, x_image_size = np.shape(data)
     x, y = position
     # logger.debug(f'{x},{y},{np.shape(data)}')
-    if y < half_size:
+    if np.logical_and(x < half_size,y < half_size):
+        cutout = data[0:y+half_size+1, 0:x+half_size+1]
+        n_xpix = half_size-y
+        n_ypix = half_size-x
+        cutout = np.pad(cutout, ((n_ypix, 0), (n_xpix, 0)), 'constant')
+
+    elif np.logical_and(x+half_size+1 > x_image_size, y+half_size+1 > y_image_size):
+        cutout = data[y - half_size: y_image_size, x-half_size, x_image_size]
+        n_xpix = (half_size+x+1) - x_image_size
+        n_ypix = (half_size+y+1) - y_image_size
+        cutout = np.pad(cutout, ((0, n_ypix), (0, n_xpix)), 'constant')
+
+    elif y < half_size:
+        logger.info(f'Cutout parameters are {y + half_size + 1}, {x - half_size}, {x + half_size + 1},{y_image_size},'
+                    f'{x_image_size}')
         cutout = data[0:y + half_size + 1, x - half_size:x + half_size + 1]
         n_pix = half_size - y
         cutout = np.pad(cutout, ((n_pix, 0), (0, 0)), 'constant')
