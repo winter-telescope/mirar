1872652bb3cacde9868970e3b970983bcbacf545
Robert Stein <rdstein@caltech.edu>
2022-07-28 16:38:42 -0700

    Add error summary

--- winterdrp/errors/__init__.py
+++ winterdrp/errors/__init__.py
@@ -1,4 +1,5 @@
 from winterdrp.paths import base_name_key
+import traceback
 from astropy.time import Time
 
 
@@ -22,3 +23,25 @@ class ErrorReport:
     def generate_log_message(self):
         return f"Error for processor {self.processor_name} at time {self.t_error} UT: " \
                f"{type(self.error).__name__} affected batch of length {len(self.contents)}."
+
+    def generate_full_traceback(self):
+        msg = f"Error for processor {self.processor_name} at time {self.t_error} UT: \n " \
+              f"{traceback.print_exception(self.error)} \n " \
+              f"This error affected the following files: {self.contents} \n"
+        return msg
+
+
+def summarise_error_reports(
+        reports: list[ErrorReport],
+        output_path=None
+) -> str:
+    summary = "",
+
+    for report in reports:
+        summary += report.generate_full_traceback()
+
+    if output_path is not None:
+        with open(output_path, "wb") as f:
+            f.write(summary)
+
+    return summary
