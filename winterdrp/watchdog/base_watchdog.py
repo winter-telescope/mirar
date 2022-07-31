52e3a8d662cad3aa83f6f434ed2b34ad4612137a
Robert Stein <rdstein@caltech.edu>
2022-07-31 16:31:28 -0700

    Actually send summary by email

--- winterdrp/watchdog/base_watchdog.py
+++ winterdrp/watchdog/base_watchdog.py
@@ -3,6 +3,8 @@ from winterdrp.errors import ErrorStack
 from winterdrp.utils.send_email import send_gmail
 import logging
 import numpy as np
+from astropy.time import Time
+from winterdrp.paths import get_output_path, base_output_dir
 
 logger = logging.getLogger(__name__)
 
@@ -19,9 +21,9 @@ class Watchdog:
     ):
         self.night = night
         self.pipeline_name = pipeline
-        self.pipeline = get_pipeline(pipeline, configuration=configuration)
+        self.pipeline = get_pipeline(pipeline, configuration=configuration, night=night)
 
-        check_email = np.sum([x is None for x in [email_recipients, email_sender]])
+        check_email = np.sum([x is not None for x in [email_recipients, email_sender]])
         if np.sum(check_email) == 1:
             err = "In order to send emails, you must specify both a a sender and a recipient. \n" \
                   f"In this case, sender is {email_sender} and recipent is {email_recipients}."
@@ -53,15 +55,36 @@ class Watchdog:
     def process_images(self):
         pass
 
-    def process_night(self):
-        batches, errorstack = self.pipeline.reduce_images([])
+    def process_full_night(self):
+        batches, errorstack = self.pipeline.reduce_images([[[], []]], catch_all_errors=True)
         self.summarise_errors(errorstack=errorstack)
 
 
 if __name__ == "__main__":
+    tonight = str(Time.now()).split(" ")[0].replace("-", "")
+
     watchdog = Watchdog(
         pipeline="summer",
-        email_sender="winter.data.reduction.password@gmail.com",
+        night=tonight,
+        email_sender="winter.data.reduction.pipeline@gmail.com",
         email_recipients=["rdstein@caltech.edu"]
     )
+
+    log_output_path = get_output_path(
+        base_name=f"{tonight}_processing_log.txt",
+        dir_root=watchdog.pipeline.night_sub_dir,
+    )
+
+    log = logging.getLogger("winterdrp")
+    #
+    handler = logging.FileHandler(log_output_path)
+    #
+    formatter = logging.Formatter('%(name)s [l %(lineno)d] - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    log.addHandler(handler)
+    log.setLevel("INFO")
+    #
+    #
+    #
+    watchdog.process_full_night()
 
