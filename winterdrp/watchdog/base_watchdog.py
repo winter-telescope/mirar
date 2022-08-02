9a78d668dec89d1ec324a8eb5f24a9e506f03ff7
Robert Stein <rdstein@caltech.edu>
2022-08-02 11:36:57 -0700

    Set log in watchdog

--- winterdrp/watchdog/base_watchdog.py
+++ winterdrp/watchdog/base_watchdog.py
@@ -21,10 +21,16 @@ class Watchdog:
             configuration: str = None,
             email_sender: str = None,
             email_recipients: str | list = None,
+            log_level="INFO"
     ):
+
         self.night = night
         self.pipeline_name = pipeline
         self.pipeline = get_pipeline(pipeline, configuration=configuration, night=night)
+
+        self.log_level = log_level
+        self.log_path = self.configure_logs(log_level)
+
         self.processed_images = []
 
         check_email = np.sum([x is not None for x in [email_recipients, email_sender]])
@@ -42,7 +48,6 @@ class Watchdog:
     def summarise_errors(
             self,
             errorstack: ErrorStack,
-            attachments: str | list[str] = None
     ):
 
         summary = errorstack.summarise_error_stack()
@@ -58,18 +63,47 @@ class Watchdog:
                 email_recipients=recipients,
                 email_subject=subject,
                 email_text=summary,
-                attachments=attachments
+                attachments=[self.log_path]
             )
+        else:
+            print(summary)
 
     def process_images(self):
         pass
 
     def process_full_night(
             self,
-            attachments: str | list[str] = None
     ):
         batches, errorstack = self.pipeline.reduce_images([[[], []]], catch_all_errors=True)
-        self.summarise_errors(errorstack=errorstack, attachments=attachments)
+        self.summarise_errors(errorstack=errorstack)
+
+    def configure_logs(self, log_level="INFO"):
+
+        log_output_path = get_output_path(
+            base_name=f"{last_night}_processing_log.txt",
+            dir_root=self.pipeline.night_sub_dir,
+        )
+
+        try:
+            os.makedirs(os.path.dirname(log_output_path))
+        except OSError:
+            pass
+
+        try:
+            os.remove(log_output_path)
+        except FileNotFoundError:
+            pass
+
+        log = logging.getLogger("winterdrp")
+
+        handler = logging.FileHandler(log_output_path)
+        formatter = logging.Formatter('%(asctime)s: %(name)s [l %(lineno)d] - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        log.addHandler(handler)
+        log.setLevel(log_level)
+
+        logger.info(f"Logging level: {self.log_level}, saving log to {log_output_path}")
+        return log_output_path
 
 
 if __name__ == "__main__":
@@ -85,28 +119,5 @@ if __name__ == "__main__":
         email_recipients=["rdstein@caltech.edu"]
     )
 
-    log_output_path = get_output_path(
-        base_name=f"{last_night}_processing_log.txt",
-        dir_root=watchdog.pipeline.night_sub_dir,
-    )
-
-    try:
-        os.makedirs(os.path.dirname(log_output_path))
-    except OSError:
-        pass
-
-    try:
-        os.remove(log_output_path)
-    except FileNotFoundError:
-        pass
-
-    log = logging.getLogger("winterdrp")
-
-    handler = logging.FileHandler(log_output_path)
-    formatter = logging.Formatter('%(asctime)s: %(name)s [l %(lineno)d] - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    log.addHandler(handler)
-    log.setLevel("INFO")
-
-    watchdog.process_full_night(attachments=[log_output_path])
+    watchdog.process_full_night()
 
