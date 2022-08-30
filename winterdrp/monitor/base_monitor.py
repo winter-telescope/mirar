4b93a8553d5351ee118ee96e665d4983a6f1a4c9
Robert Stein <rdstein@caltech.edu>
2022-08-30 12:46:53 -0700

    Add extra line at end of email

--- winterdrp/monitor/base_monitor.py
+++ winterdrp/monitor/base_monitor.py
@@ -122,7 +122,7 @@ class Monitor:
     ):
 
         error_summary = errorstack.summarise_error_stack(verbose=False)
-        summary = f"Processed a total of {len(self.processed_science)} science images. \n\n" + error_summary
+        summary = f"Processed a total of {len(self.processed_science)} science images. \n\n" + error_summary + " \n"
 
         logger.info(f"Writing error log to {self.error_path}")
         with open(self.error_path, "w") as f:
