aa7805e111242281b8717afd05ea6c4f6ceec54f
Robert Stein <rdstein@caltech.edu>
2022-08-30 15:58:19 -0700

    Spinoff load_raw_summer_image

--- winterdrp/pipelines/summer/summer_pipeline.py
+++ winterdrp/pipelines/summer/summer_pipeline.py
@@ -2,29 +2,25 @@ import logging
 import os
 import astropy.io.fits
 import numpy as np
-from astropy.coordinates import SkyCoord
-import astropy.units as u
+import pkg_resources
+from astropy.io import fits
+
 from winterdrp.pipelines.base_pipeline import Pipeline
 from winterdrp.downloader.caltech import download_via_ssh
-from astropy.time import Time
 from winterdrp.catalog import Gaia2Mass, PS1, SDSS
 from winterdrp.processors.database.database_exporter import DatabaseImageExporter
 from winterdrp.processors.astromatic.sextractor.sextractor import sextractor_header_key
 from winterdrp.processors.autoastrometry import AutoAstrometry
 from winterdrp.processors.astromatic import Sextractor, Scamp, Swarp, PSFex
-from astropy.io import fits
-from winterdrp.pipelines.summer.summer_files import get_summer_schema_path, summer_mask_path, summer_weight_path, \
+from winterdrp.pipelines.summer.summer_files import get_summer_schema_path, summer_weight_path, \
     sextractor_astrometry_config, sextractor_photometry_config, scamp_path, swarp_path
 from winterdrp.pipelines.summer.summer_files.schema import summer_schema_dir
-from winterdrp.processors.utils import ImageSaver, HeaderAnnotator, ImageLoader, ImageSelector, ImageBatcher
+from winterdrp.processors.utils import ImageSaver, ImageLoader, ImageSelector, ImageBatcher
 from winterdrp.processors.utils.cal_hunter import CalHunter, CalRequirement
-from winterdrp.processors.utils.image_rejector import ImageRejector
 from winterdrp.processors.photcal import PhotCalibrator
-from winterdrp.processors import MaskPixels, BiasCalibrator, FlatCalibrator
+from winterdrp.processors import BiasCalibrator, FlatCalibrator
 from winterdrp.processors.csvlog import CSVLog
-from winterdrp.paths import core_fields, base_name_key, latest_save_key, raw_img_key
-
-# Image subtraction modules
+from winterdrp.paths import core_fields, base_name_key
 from winterdrp.processors.reference import Reference
 from winterdrp.processors.zogy.zogy import ZOGY, ZOGYPrepare, default_summer_catalog_purifier
 from winterdrp.references.ps1 import PS1Ref
@@ -33,7 +29,7 @@ from winterdrp.processors.candidates.candidate_detector import DetectCandidates
 from winterdrp.processors.photometry.psf_photometry import PSFPhotometry
 from winterdrp.processors.photometry.aperture_photometry import AperturePhotometry
 from winterdrp.processors.candidates.utils import RegionsWriter, DataframeWriter
-import pkg_resources
+from winterdrp.pipelines.summer.load_summer_image import load_raw_summer_image
 
 summer_flats_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 summer_gain = 1.0
@@ -70,138 +66,6 @@ def summer_photometric_catalog_generator(
         return SDSS(min_mag=10, max_mag=20, search_radius_arcmin=7.5, filter_name=filter_name)
     else:
         return PS1(min_mag=10, max_mag=20, search_radius_arcmin=7.5, filter_name=filter_name)
-
-
-def load_raw_summer_image(
-        path: str
-) -> tuple[np.array, astropy.io.fits.Header]:
-    with fits.open(path) as data:
-        header = data[0].header
-        header["OBSCLASS"] = ["calibration", "science"][header["OBSTYPE"] == "SCIENCE"]
-        header['UTCTIME'] = header['UTCSHUT']
-        header['TARGET'] = header['OBSTYPE'].lower()
-
-        crd = SkyCoord(ra=data[0].header['RA'], dec=data[0].header['DEC'], unit=(u.deg, u.deg))
-        header['RA'] = crd.ra.deg
-        header['DEC'] = crd.dec.deg
-
-        header['CRVAL1'] = header['RA']
-        header['CRVAL2'] = header['DEC']
-
-        tel_crd = SkyCoord(ra=data[0].header['TELRA'], dec=data[0].header['TELDEC'], unit=(u.deg, u.deg))
-        header['TELRA'] = tel_crd.ra.deg
-        header['TELDEC'] = tel_crd.dec.deg
-        header['BZERO'] = 0
-
-        header[latest_save_key] = path
-        header[raw_img_key] = path
-
-        data[0].data = data[0].data * 1.0
-
-        if 'other' in header['FILTERID']:
-            header['FILTERID'] = 'r'
-
-        header["CALSTEPS"] = ""
-
-        base_name = os.path.basename(path)
-        header[base_name_key] = base_name
-        header["EXPID"] = int("".join(base_name.split("_")[1:3])[2:])
-        # header["EXPID"] = str(header["NIGHT"]) + str(header["OBSHISTID"])
-
-        pipeline_version = pkg_resources.require("winterdrp")[0].version
-        pipeline_version_padded_str = "".join([x.rjust(2, "0") for x in pipeline_version.split(".")])
-        header["PROCID"] = int(str(header["EXPID"])+str(pipeline_version_padded_str))
-        header.append(('GAIN', summer_gain, 'Gain in electrons / ADU'), end=True)
-
-        header['OBSDATE'] = int(header['UTC'].split('_')[0])
-
-        obstime = Time(header['UTCISO'], format='iso')
-        t0 = Time('2018-01-01', format='iso')
-        #header['NIGHT'] = np.floor((obstime - t0).jd).astype(int)
-        header['NIGHT'] = int(obstime.jd) - int(t0.jd)
-        header['EXPMJD'] = header['OBSMJD']
-
-        default_id = 0
-        
-        for key in ["PROGID", "OBSID"]:
-            if key not in header.keys():
-                # logger.warning(f"No {key} found in header of {path}")
-                header[key] = default_id
-            else:
-                try:
-                    header[key] = int(header[key])
-                except ValueError:
-                    header[key] = default_id
-
-        if "SUBPROG" not in header.keys():
-            # logger.warning(f"No SUBPROG found in header of {path}")
-            header['SUBPROG'] = 'none'
-
-        header['FILTER'] = header['FILTERID']
-        try:
-            header['SHUTOPEN'] = Time(header['SHUTOPEN'], format='iso').jd
-        except ValueError:
-            logger.warning(f"Error parsing 'SHUTOPEN' of {path}: ({header['SHUTOPEN']})")
-
-        try:
-            header['SHUTCLSD'] = Time(header['SHUTCLSD'], format='iso').jd
-        except ValueError:
-            logger.warning(f"Error parsing 'SHUTCLSD' of {path}: ({header['SHUTCLSD']})")
-
-        header['PROCFLAG'] = 0
-        sunmoon_keywords = [
-            'MOONRA', 'MOONDEC', 'MOONILLF', 'MOONPHAS', 'MOONALT', 'SUNAZ', 'SUNALT',
-        ]
-        for key in sunmoon_keywords:
-            val = 0
-            if key in header.keys():
-                if header[key] not in ['']:
-                    val = header[key]
-            header[key] = val
-
-        itid_dict = {
-            'SCIENCE': 1,
-            'BIAS': 2,
-            'FLAT': 2,
-            'DARK': 2,
-            'FOCUS': 3,
-            'POINTING': 4,
-            'OTHER': 5
-        }
-
-        if not header['OBSTYPE'] in itid_dict.keys():
-            header['ITID'] = 5
-        else:
-            header['ITID'] = itid_dict[header['OBSTYPE']]
-
-        if header['FIELDID'] == 'radec':
-            header['FIELDID'] = 999999999
-
-        if header['ITID'] != 1:
-            header['FIELDID'] = -99
-
-        if 'COADDS' not in header.keys():
-            header['COADDS'] = 1
-
-        if header['PROGID'] in ['', 'WINTER']:
-            header['PROGID'] = 2
-
-        try:
-            header['PROGID'] = int(header['PROGID'])
-        except ValueError:
-            try:
-                progpi = header['PROGID']
-                header['PROGID'] = int(header['PROGPI'])
-                header['PROGPI'] = progpi
-            except KeyError:
-                header['PROGID'] = 0
-
-        crds = SkyCoord(ra=header['RA'], dec=header['DEC'], unit=(u.deg, u.deg))
-        header['RA'] = crds.ra.deg
-        header['DEC'] = crds.dec.deg
-
-        data[0].header = header
-    return data[0].data, data[0].header
 
 
 def load_proc_summer_image(
