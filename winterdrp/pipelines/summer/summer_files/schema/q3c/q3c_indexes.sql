dff23c3a75560502e669d0dc9468147b38c7140e
viraj21197 <karambelkarvraj21197@gmail.com>
2022-09-01 14:55:39 -0700

    add correct syntax for q3c indexing

--- winterdrp/pipelines/summer/summer_files/schema/q3c/q3c_indexes.sql
+++ winterdrp/pipelines/summer/summer_files/schema/q3c/q3c_indexes.sql
@@ -1,5 +1,5 @@
 CREATE EXTENSION q3c;
 
-CREATE INDEX ON raw (q3c_ang2pix(ra, dec));
-CLUSTER raw_q3c_ang2pix_idx ON raw;
+CREATE INDEX IF NOT EXISTS ON raw (q3c_ang2ipix(ra, dec));
+CLUSTER raw_q3c_ang2ipix_idx ON raw;
 ANALYZE raw;
