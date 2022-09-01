63599bcd108da6d4f3e33e3acab2eb25297620d3
viraj21197 <karambelkarvraj21197@gmail.com>
2022-09-01 14:59:51 -0700

    fix typo

--- winterdrp/pipelines/summer/summer_files/schema/q3c/q3c_indexes.sql
+++ winterdrp/pipelines/summer/summer_files/schema/q3c/q3c_indexes.sql
@@ -1,5 +1,5 @@
 CREATE EXTENSION q3c;
 
-CREATE INDEX IF NOT EXISTS ON raw (q3c_ang2ipix(ra, dec));
+CREATE INDEX IF NOT EXISTS raw_q3c_ang2ipix_idx ON raw (q3c_ang2ipix(ra, dec));
 CLUSTER raw_q3c_ang2ipix_idx ON raw;
 ANALYZE raw;
