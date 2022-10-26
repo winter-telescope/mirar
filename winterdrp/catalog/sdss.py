6ec0219321102c3ac32b4cb3435846bc7ebc6d21
Robert Stein <rdstein@caltech.edu>
2022-10-26 15:00:00 -0400

    New SDSS class and error

--- winterdrp/catalog/sdss.py
+++ winterdrp/catalog/sdss.py
@@ -1,62 +1,38 @@
 import logging
 import astropy.table
-from astroquery.vizier import Vizier
-from winterdrp.catalog.base_catalog import BaseCatalog
+from winterdrp.catalog.base_catalog import VizierCatalog
 from astropy.coordinates import SkyCoord
 import astropy.units as u
 from astropy.table import Table
+from winterdrp.errors import ProcessorError
+import randomsdss
 
 logger = logging.getLogger(__name__)
 
 
-class SDSS(BaseCatalog):
+class NotInSDSSError(ProcessorError):
+    pass
+
+
+def in_sdss(
+        ra_deg: float,
+        dec_deg: float
+) -> bool:
+    dr16 = randomsdss.DR16()
+    return dr16.contains(ra_deg, dec_deg)
+
+
+class SDSS(VizierCatalog):
 
     catalog_vizier_code = "V/154"
     abbreviation = "sdss"
 
-    def __init__(
-            self,
-            search_radius_arcmin: float,
-            min_mag: float,
-            max_mag: float,
-            filter_name: str,
-            snr_threshold: float = 3.0
-    ):
-        super().__init__(search_radius_arcmin, min_mag, max_mag, filter_name)
-        self.snr_threshold = snr_threshold
+    ra_key = "RA_ICRS"
+    dec_key = "DE_ICRS"
 
-    def get_catalog(
-            self,
-            ra_deg: float,
-            dec_deg: float
-    ) -> astropy.table.Table:
-
-        logger.info(
-            f'Querying SDSS catalog around RA {ra_deg:.4f}, '
-            f'Dec {dec_deg:.4f} with a radius of {self.search_radius_arcmin:.4f} arcmin'
-        )
-
-        v = Vizier(columns=['*'],
-                   column_filters={f"{self.filter_name}mag" : f"< {self.max_mag}",
-                                   f"e_{self.filter_name}mag" : "<%.3f" % (1.086 / self.snr_threshold)},
-                   row_limit=-1)
-
-        query = v.query_region(
-            SkyCoord(ra=ra_deg, dec=dec_deg, unit=(u.deg, u.deg)),
-            radius=str(self.search_radius_arcmin) + 'm',
-            catalog=self.catalog_vizier_code,
-            cache=False
-        )
-
-        if len(query) == 0:
-            logger.info('No matches found in the given radius in SDSS')
-            t = Table()
-        else:
-            t = query[0]
-            t['ra'] = t['RA_ICRS']
-            t['dec'] = t['DE_ICRS']
-            t['magnitude'] = t[f'{self.filter_name}mag']
-            logger.info(f'{len(t)} matches found in the given radius in SDSS')
-            t.write('phot_table.csv', overwrite=True)
-            t = Table.read('phot_table.csv')
-        return t
+    @staticmethod
+    def check_coverage(ra_deg: float, dec_deg: float):
+        if not in_sdss(ra_deg, dec_deg):
+            err = f"Querying for SDSS sources, but the field ({ra_deg}, {dec_deg}) was not observed in SDSS."
+            logger.error(err)
+            raise NotInSDSSError(err)
