ae1b4e11277ec1438d0b595e43d41a01fbc0183d
Robert Stein <rdstein@caltech.edu>
2022-10-28 17:17:12 -0400

    Use pydl mangle

--- winterdrp/catalog/sdss.py
+++ winterdrp/catalog/sdss.py
@@ -1,11 +1,14 @@
 import logging
-import astropy.table
+from pathlib import Path
+
+import numpy as np
+from pydl.pydlutils import mangle
+import requests
+
 from winterdrp.catalog.base_catalog import VizierCatalog
-from astropy.coordinates import SkyCoord
-import astropy.units as u
-from astropy.table import Table
 from winterdrp.errors import ProcessorError
-import randomsdss
+from winterdrp.paths import base_output_dir
+
 
 logger = logging.getLogger(__name__)
 
@@ -14,12 +17,36 @@ class NotInSDSSError(ProcessorError):
     pass
 
 
+SDSS_RELEASE = "dr16"
+
+SDSS_COVERAGE_PATH = Path(base_output_dir).joinpath(f"SDSS/{SDSS_RELEASE}_window.ply")
+
+SDSS_COVERAGE_URL = f"https://data.sdss.org/sas/{SDSS_RELEASE}/sdss/tiling/final/internal/tmp_window_unif.ply"
+
+
+def get_sdss_coverage(
+) -> mangle.PolygonList:
+
+    if not SDSS_COVERAGE_PATH.parent.exists():
+        SDSS_COVERAGE_PATH.parent.mkdir(parents=True)
+
+    if not SDSS_COVERAGE_PATH.is_file():
+        logger.info(f"No coverage found. Downloading SDSS coverage map from {SDSS_COVERAGE_URL}")
+        print(SDSS_COVERAGE_PATH)
+        with open(str(SDSS_COVERAGE_PATH), "wb+") as f:
+            r = requests.get(SDSS_COVERAGE_URL, allow_redirects=True)
+            f.write(r.content)
+
+    sdss_coverage = mangle.read_mangle_polygons(str(SDSS_COVERAGE_PATH))
+    return sdss_coverage
+
+
 def in_sdss(
         ra_deg: float,
         dec_deg: float
 ) -> bool:
-    dr16 = randomsdss.DR16()
-    return dr16.contains(ra_deg, dec_deg)
+    sdss_coverage = get_sdss_coverage()
+    return mangle.is_in_window(sdss_coverage, np.array([[ra_deg, dec_deg]]))[0]
 
 
 class SDSS(VizierCatalog):
