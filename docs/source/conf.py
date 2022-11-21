476bc51aacc615328b6291346fe3d682088d3325
Robert Stein <rdstein@caltech.edu>
2022-11-20 21:00:22 -0800

    updat

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'winterdrp'
+copyright = '2022, Stein'
+author = 'Stein'
 
 release = '0.1'
 version = '0.1.0'
