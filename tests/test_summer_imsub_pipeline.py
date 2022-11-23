cac2f21abf7cc3d2673f577aa25e82fe09ef60d6
Robert Stein <rdstein@caltech.edu>
2022-11-22 16:07:51 -0800

    delete redundant imports

--- tests/test_summer_imsub_pipeline.py
+++ tests/test_summer_imsub_pipeline.py
@@ -1,16 +1,11 @@
 import unittest
 import logging
-from winterdrp.processors.utils.image_loader import ImageLoader
-from winterdrp.processors.utils.image_selector import ImageSelector
-from winterdrp.paths import base_name_key
 from winterdrp.pipelines.summer.summer_pipeline import SummerPipeline
-from winterdrp.downloader.get_test_data import get_test_data_dir
-from winterdrp.pipelines.summer.blocks import subtract
-from winterdrp.pipelines.summer.load_summer_image import load_proc_summer_image
 from winterdrp.data import Dataset, ImageBatch
 
 logger = logging.getLogger(__name__)
 
+logging.basicConfig(level=logging.INFO)
 
 expected_values = {
     'SCORSTD': 1.120988782614284,
@@ -31,7 +26,7 @@ class TestSummerPipeline(unittest.TestCase):
 
         res, errorstack = pipeline.reduce_images(Dataset([ImageBatch()]), catch_all_errors=False)
 
-        self.assertEqual(len(res[0][0]), 1)
+        self.assertEqual(len(res), 1)
 
         header = res[0][0].get_header()
 
@@ -42,23 +37,4 @@ class TestSummerPipeline(unittest.TestCase):
                 self.assertEqual(value, header[key])
             else:
                 raise TypeError(f"Type for value ({type(value)} is neither float not int.")
-
-
-if __name__ == "__main__":
-
-    print("Calculating latest scorr metrics dictionary")
-
-    # Code to generate updated ZP dict of the results change
-
-    new_res, new_errorstack = pipeline.reduce_images([[[], []]], catch_all_errors=False)
-
-    new_header = new_res[0][1][0]
-
-    new_exp = "expected_values = { \n"
-    for header_key in new_header.keys():
-        if "SCOR" in header_key:
-            new_exp += f'    "{header_key}": {new_header[header_key]}, \n'
-    new_exp += "}"
-    print(new_exp)
-
 
