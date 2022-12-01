a12fdb0875218b252949d7fd8bcec82d3dd40467
Robert Stein <rdstein@caltech.edu>
2022-12-01 15:27:30 -0800

    Doc data

--- winterdrp/data/__init__.py
+++ winterdrp/data/__init__.py
@@ -1,3 +1,5 @@
+"""Module to specify the input data classes for ..module::wintedrp.provcessors
+"""
 from winterdrp.data.base_data import DataBatch, DataBlock, Dataset
 from winterdrp.data.image_data import Image, ImageBatch
 from winterdrp.data.source_data import SourceBatch, SourceTable
