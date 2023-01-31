ad20be0082d9100e0ca1ec455ce61fbeb7c3ccee
viraj21197 <karambelkarvraj21197@gmail.com>
2023-01-31 13:14:33 -0800

    working LACosmic ray processor

--- winterdrp/processors/cosmic_rays.py
+++ winterdrp/processors/cosmic_rays.py
@@ -1,3 +1,5 @@
+import logging
+
 from winterdrp.processors.base_processor import BaseImageProcessor
 from winterdrp.data import ImageBatch
 import lacosmic
@@ -5,6 +7,9 @@ from winterdrp.paths import latest_mask_save_key, exptime_key
 from winterdrp.data import Image
 import numpy as np
 from winterdrp.errors import NoncriticalProcessingError
+
+
+logger = logging.getLogger(__name__)
 
 
 class CRCleanError(NoncriticalProcessingError):
@@ -52,7 +57,7 @@ class LACosmicCleaner(BaseImageProcessor):
                   'or the header key to use for the readnoise. '
             raise CRCleanError(err)
 
-        if np.logical_and(self.error is None, np.logical_or(self.readnoise is None, self.effective_gain is None)):
+        if np.logical_and(self.error is None, ((self.readnoise is None) & (self.effective_gain is None) & (self.effective_gain_key is None))):
             err = 'LA Cosmic cleaner requires either a 2D error image, or the readnoise and the effective gain. ' \
                   'Please make sure at least one is provided '
             raise CRCleanError(err)
@@ -66,17 +71,26 @@ class LACosmicCleaner(BaseImageProcessor):
             if self.min_exptime is not None:
                 if image[exptime_key]<self.min_exptime:
                     run_crclean = False
+                    logger.info(f"Exposure time is smaller than minimum specified, skipping cosmic ray cleaner.")
 
             if run_crclean:
-                maskpath = image[latest_mask_save_key]
-                mask_data = self.open_fits(maskpath)
+                logger.info(f"Running LACosmic")
+                if latest_mask_save_key in image.header.keys():
+                    maskpath = image.header[latest_mask_save_key]
+                    mask_image = self.open_fits(maskpath)
+                    mask_data = ~np.array(mask_image.data, dtype=bool)
+                else:
+                    logger.warning("No mask path provided in image header. Masking only nans in the image, assuming "
+                                   "any other masking has been done in ImageLoader.")
+                    mask_data = (np.isnan(image.get_data())).astype(bool)
+                logger.info(f"{~(mask_data)}")
                 effective_gain, readnoise = self.effective_gain, self.readnoise
                 if effective_gain is None:
-                    effective_gain = image[self.effective_gain_key]
+                    effective_gain = image.header[self.effective_gain_key]
 
                 if readnoise is None:
-                    readnoise = image[self.readnoise_key]
-
+                    readnoise = image.header[self.readnoise_key]
+                logger.info(f"Cleaning cosmic rays")
                 cleaned_data, cr_mask = lacosmic.lacosmic(image.get_data(),
                                                           contrast=self.contrast,
                                                           cr_threshold=self.cr_threshold,
@@ -89,6 +103,7 @@ class LACosmicCleaner(BaseImageProcessor):
                                                           maxiter=self.maxiter,
                                                           border_mode=self.border_mode
                                                           )
+                logger.info(f"LACosmic finished cleaning")
                 image.set_data(cleaned_data)
                 image['history'] = f"Cleaned with LACosmic version {lacosmic.__version__}"
         return batch
