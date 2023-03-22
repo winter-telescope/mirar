9e4e004d70a6e2fbb256602a682382cd5a8f8660
viraj21197 <karambelkarvraj21197@gmail.com>
2023-03-22 00:36:47 -0700

    add exists to sql

--- winterdrp/pipelines/summer/models/basemodel.py
+++ winterdrp/pipelines/summer/models/basemodel.py
@@ -5,10 +5,12 @@ from datetime import date
 from typing import ClassVar
 
 from pydantic import BaseModel, Extra, Field, root_validator, validator
-from sqlalchemy import Insert, Select, Table
+from sqlalchemy import Insert, Select, Table, inspect, select
 from sqlalchemy.orm import DeclarativeBase
 
 from winterdrp.utils.sql import get_engine
+
+db_name = "summertest"
 
 
 class PydanticBase(BaseModel):
@@ -66,11 +68,14 @@ class BaseDB(PydanticBase):
 
         :return: None
         """
-        engine = get_engine()
+        stmt = Insert(self.sql_model).values(**self.dict())
+
+        engine = get_engine(db_name=self.sql_model.db_name)
         with engine.connect() as conn:
-            with conn.begin():
-                stmt = Insert(self.sql_model).values(**self.dict())
-                conn.execute(stmt)
+            res = conn.execute(stmt)
+            conn.commit()
+
+        return res
 
     def insert_entry(self):
         """
@@ -80,33 +85,80 @@ class BaseDB(PydanticBase):
         """
         self._insert_entry()
 
+    # def exists(self, key=None) -> bool:
+    #     """
+    #     Checks if the pydantic-ified data exists the corresponding sql database
+    #
+    #     :return: bool
+    #     """
+    #     if key is None:
+    #         key = self.sql_model().get_primary_key()
+    #
+    #     if key not in self.__dict__.keys():
+    #         err = f"Key {key} not present in the {self.__class__} fields." \
+    #               f"Either provide a different key, or consider using the exists " \
+    #               f"function in {self.sql_model} instead"
+    #         raise ValueError(err)
+    #     print(f"Pydantic existential check")
+    #     return self.sql_model().exists(value=self.__dict__[key], key=key)
+
 
 class Base(DeclarativeBase):
     """
     Parent class for databases
     """
 
+    db_name = db_name
 
-def _execute(stmt):
+    def get_primary_key(self) -> str:
+        return inspect(self.__class__).primary_key[0].name
+
+    def exists(self, value, key: str = None) -> bool:
+        """
+        Function to query a table and see whether an entry with key==value exists.
+        If key is None, key will default to the table's primary key.
+        Args:
+            value: = Value to match
+            key: str = column name
+
+        Returns:
+            boolean
+        """
+        engine = get_engine(db_name=self.db_name)
+        if key is None:
+            key = self.get_primary_key()
+        else:
+            columns = inspect(self.__class__).mapper.column_attrs
+            column_names = [c.key for c in columns]
+            if key not in column_names:
+                err = f"{self.__tablename__} has no column {key}"
+                raise ValueError(err)
+
+        print(f"Checking if {key}={value} exists in {self.__tablename__}")
+        stmt = select(self.__class__).where(self.__class__.__dict__[key] == value)
+
+        return _exists(stmt, engine=engine)
+
+
+def _execute(stmt, engine):
     """
     Checks if the pydantic-ified data exists the corresponding sql database
 
     :return: bool
     """
-    engine = get_engine()
     with engine.connect() as conn:
         with conn.begin():
             res = conn.execute(stmt).fetchall()
     return res
 
 
-def _exists(stmt: Select) -> bool:
+def _exists(stmt: Select, engine) -> bool:
     """
     Checks if the pydantic-ified data exists the corresponding sql database
 
     :return: bool
     """
-    return len(_execute(stmt)) > 0
+    return len(_execute(stmt, engine)) > 0
 
 
 ra_field: float = Field(title="RA (degrees)", ge=0.0, le=360.0)
