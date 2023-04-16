687ae4ce50e70488ed1fc35c034d8ada4911b019
viraj21197 <karambelkarvraj21197@gmail.com>
2023-04-16 03:14:01 -0700

    update to other changes for testing

--- winterdrp/pipelines/reference_building/ir_refbuild_pipeline.py
+++ winterdrp/pipelines/reference_building/ir_refbuild_pipeline.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from typing import Callable
 
@@ -6,26 +7,25 @@ import pandas as pd
 from winterdrp.catalog import Gaia2Mass
 from winterdrp.data import Image, ImageBatch
 from winterdrp.pipelines.base_pipeline import Pipeline
+from winterdrp.pipelines.reference_building.db_models import RefComponents, RefStacks
 from winterdrp.pipelines.wirc.load_wirc_image import load_raw_wirc_image
 from winterdrp.pipelines.wirc.wirc_files import sextractor_astrometry_config
 from winterdrp.processors.astromatic.sextractor.sextractor import Sextractor
 from winterdrp.processors.astromatic.swarp.swarp import Swarp
 from winterdrp.processors.base_processor import BaseImageProcessor
+from winterdrp.processors.candidates.utils import get_image_center_wcs_coords
 from winterdrp.processors.photcal import PhotCalibrator
+from winterdrp.processors.sqldatabase.database_exporter import DatabaseImageExporter
 from winterdrp.processors.utils import ImageLoader, ImageSaver
 from winterdrp.references import BaseReferenceGenerator
 from winterdrp.references.ukirt import UKIRTRef
-from winterdrp.pipelines.reference_building.db_models import RefComponents, RefStacks
-from winterdrp.processors.sqldatabase.database_exporter import DatabaseImageExporter
-from winterdrp.processors.candidates.utils import get_image_center_wcs_coords
-import logging
 
 logger = logging.getLogger(__name__)
 
 refbuild_dir = os.path.dirname(__file__)
 
-astromatic_config_dir = os.path.join(refbuild_dir, "config/files")
-swarp_config_path = os.path.join(astromatic_config_dir, "swarp.config")
+astromatic_config_dir = os.path.join(refbuild_dir, "config/")
+swarp_config_path = os.path.join(astromatic_config_dir, "config.swarp")
 
 
 def winter_reference_generator(image: Image):
@@ -36,7 +36,7 @@ def winter_reference_generator(image: Image):
         swarp_resampler=winter_reference_image_resampler,
         phot_calibrator=winter_reference_phot_calibrator,
         num_query_points=4,
-        db_table=RefComponents
+        db_table=RefComponents,
     )
 
 
@@ -53,7 +53,10 @@ def winter_reference_image_resampler(**kwargs) -> Swarp:
     :param kwargs: kwargs
     :return: Swarp processor
     """
-    return Swarp(swarp_config_path=swarp_config_path, cache=True, **kwargs)
+    logger.info(kwargs)
+    return Swarp(
+        swarp_config_path=swarp_config_path, subtract_bkg=True, cache=True, **kwargs
+    )
 
 
 def wirc_photometric_catalog_generator(image: Image) -> Gaia2Mass:
@@ -65,7 +68,11 @@ def wirc_photometric_catalog_generator(image: Image) -> Gaia2Mass:
     """
     filter_name = image["FILTER"]
     return Gaia2Mass(
-        min_mag=10, max_mag=20, search_radius_arcmin=30, filter_name=filter_name
+        min_mag=10,
+        max_mag=20,
+        search_radius_arcmin=10,
+        filter_name=filter_name,
+        snr_threshold=20,
     )
 
 
@@ -83,8 +90,6 @@ def winter_reference_phot_calibrator(image: Image, **kwargs) -> PhotCalibrator:
 
 class MakeDummyImages(BaseImageProcessor):
     pass
-
-
 
 
 class GetReferenceImage(BaseImageProcessor):
@@ -109,8 +114,8 @@ class GetReferenceImage(BaseImageProcessor):
             ra_cent, dec_cent = get_image_center_wcs_coords(image=image)
             logger.debug(ra_cent)
             logger.debug(dec_cent)
-            ref_image['RA_CENT'] = ra_cent
-            ref_image['DEC_CENT'] = dec_cent
+            ref_image["RA_CENT"] = ra_cent
+            ref_image["DEC_CENT"] = dec_cent
             ref_batch.append(ref_image)
 
         return ref_batch
@@ -130,13 +135,18 @@ class IRRefBuildPipeline(Pipeline):
             write_regions_bool=True,
             cache=True
         ),
-        PhotCalibrator(ref_catalog_generator=wirc_photometric_catalog_generator,
-                       write_regions=True),
+        PhotCalibrator(
+            ref_catalog_generator=wirc_photometric_catalog_generator,
+            write_regions=True,
+            x_lower_limit=0,
+            fwhm_threshold_arcsec=3,
+        ),
         ImageSaver(output_dir_name="stacked_ref"),
-        DatabaseImageExporter(db_table=RefStacks,
-                              duplicate_protocol="replace",
-                              q3c_bool=False,
-                              )
+        DatabaseImageExporter(
+            db_table=RefStacks,
+            duplicate_protocol="replace",
+            q3c_bool=False,
+        ),
     ]
 
     all_pipeline_configurations = {"default": refbuild}
