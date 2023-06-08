b17006b484c1c2acac899d76d78ce6bf3271fef4
viraj21197 <karambelkarvraj21197@gmail.com>
2023-06-08 13:56:19 -0700

    lint

--- mirar/processors/__init__.py
+++ mirar/processors/__init__.py
@@ -3,7 +3,7 @@ from mirar.processors.base_processor import BaseImageProcessor
 from mirar.processors.bias import BiasCalibrator
 from mirar.processors.dark import DarkCalibrator
 from mirar.processors.flat import FlatCalibrator, SkyFlatCalibrator
-from mirar.processors.mask import MaskPixels
+from mirar.processors.mask import MaskPixelsFromPath
 from mirar.processors.utils.image_saver import ImageSaver
 
 #
@@ -16,7 +16,8 @@ from mirar.processors.utils.image_saver import ImageSaver
 #         processor = BaseProcessor.subclasses[processor_name]
 #     except KeyError:
 #         err = f"Processor type '{processor_name}' not recognised. " \
-#               f"The following processors are available: {BaseProcessor.subclasses.keys()}"
+#               f"The following processors are available:
+#               {BaseProcessor.subclasses.keys()}"
 #         logger.error(err)
 #         raise KeyError(err)
 #
