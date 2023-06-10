a31f6576718f33d664157c26759310fd8caf316b
viraj21197 <karambelkarvraj21197@gmail.com>
2023-06-09 21:24:41 -0700

    better purifier

--- mirar/pipelines/wirc/generator.py
+++ mirar/pipelines/wirc/generator.py
@@ -24,13 +24,21 @@ def wirc_photometric_img_catalog_purifier(catalog, image):
 
     :return: purified catalog
     """
+    edge_width_pixels = 300
+    fwhm_threshold_arcsec = 4.0
+
+    x_lower_limit = edge_width_pixels
+    x_upper_limit = image.get_data().shape[1] - edge_width_pixels
+    y_lower_limit = edge_width_pixels
+    y_upper_limit = image.get_data().shape[0] - edge_width_pixels
+
     clean_mask = (
         (catalog["FLAGS"] == 0)
-        & (catalog["FWHM_WORLD"] < 4.0 / 3600.0)
-        & (catalog["X_IMAGE"] > 200)
-        & (catalog["X_IMAGE"] < 1800)
-        & (catalog["Y_IMAGE"] > 200)
-        & (catalog["Y_IMAGE"] < 1800)
+        & (catalog["FWHM_WORLD"] < fwhm_threshold_arcsec / 3600.0)
+        & (catalog["X_IMAGE"] > x_lower_limit)
+        & (catalog["X_IMAGE"] < x_upper_limit)
+        & (catalog["Y_IMAGE"] > y_lower_limit)
+        & (catalog["Y_IMAGE"] < y_upper_limit)
     )
 
     return catalog[clean_mask]
@@ -58,6 +66,8 @@ def wirc_photometric_catalog_generator(image: Image) -> Gaia2Mass:
         max_mag=20,
         search_radius_arcmin=10,
         filter_name=filter_name,
+        acceptable_h_ph_quals=["A"],
+        acceptable_k_ph_quals=["A"],
     )
 
 
