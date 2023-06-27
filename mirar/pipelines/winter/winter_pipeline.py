6fc5574a7eebe07b72675bf544bd9be29c2f4715
viraj21197 <karambelkarvraj21197@gmail.com>
2023-06-27 17:10:38 -0500

    winter pipeline

--- mirar/pipelines/winter/winter_pipeline.py
+++ mirar/pipelines/winter/winter_pipeline.py
@@ -3,20 +3,46 @@ Module with pipeline for building reference images in the IR from WFAU
 """
 import logging
 
+from mirar.downloader.caltech import download_via_ssh
 from mirar.pipelines.base_pipeline import Pipeline
-from mirar.pipelines.winter.blocks import refbuild
+from mirar.pipelines.winter.blocks import (
+    commissioning,
+    commissioning_dark,
+    commissioning_flat,
+    commissioning_multiboard_stack,
+    commissioning_noise,
+    commissioning_proc,
+    commissioning_reduce,
+    commissioning_stack,
+    full_commissioning,
+    log,
+    refbuild,
+)
+from mirar.pipelines.winter.config import PIPELINE_NAME
 
 logger = logging.getLogger(__name__)
 
 
 class WINTERPipeline(Pipeline):
     """
-    Pipeline for building reference images in the IR from WFAU
+    Pipeline for processing WINTER data
     """
 
     name = "winter"
 
-    all_pipeline_configurations = {"refbuild": refbuild}
+    all_pipeline_configurations = {
+        "refbuild": refbuild,
+        "commissioning": commissioning,
+        "log": log,
+        "commissioning_dark": commissioning_dark,
+        "commissioning_stack": commissioning_stack,
+        "commissioning_flat": commissioning_flat,
+        "commissioning_reduce": commissioning_reduce,
+        "commissioning_proc": commissioning_proc,
+        "commissioning_multiboard_stack": commissioning_multiboard_stack,
+        "full_commissioning": full_commissioning,
+        "commissioning_noise": commissioning_noise,
+    }
 
     gain = 1.0
     non_linear_level = 65535
@@ -27,4 +53,10 @@ class WINTERPipeline(Pipeline):
 
     @staticmethod
     def download_raw_images_for_night(night: str):
-        pass
+        download_via_ssh(
+            server="winter.caltech.edu",
+            base_dir="/data/loki/raw_data/winter",
+            night=night,
+            pipeline=PIPELINE_NAME,
+            server_sub_dir="raw",
+        )
