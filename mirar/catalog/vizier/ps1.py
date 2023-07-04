0d5b8c86b8c6e8de0a6fac216b3e76629cc2fa47
Viraj Karambelkar <karambelkarvraj21197@gmail.com>
2023-07-04 12:16:49 -0400

    Remove saturated sources from PS1 (#440)

--- mirar/catalog/vizier/ps1.py
+++ mirar/catalog/vizier/ps1.py
@@ -3,6 +3,9 @@ Module for querying PS1 catalog
 """
 import logging
 
+import numpy as np
+from astropy.table import Table
+
 from mirar.catalog.vizier.base_vizier_catalog import VizierCatalog
 
 logger = logging.getLogger(__name__)
@@ -18,3 +21,15 @@ class PS1(VizierCatalog):
 
     ra_key = "RAJ2000"
     dec_key = "DEJ2000"
+
+    def filter_catalog(self, table: Table) -> Table:
+        logger.info(f"original ps1 table length: {len(table)}")
+        logger.info("removing ps1 sources with SATURATED flag...")
+        sat_flag = 4096  # SATURATED value
+        column = table[str(self.filter_name) + "Flags"]
+        check = (column & sat_flag) / sat_flag
+        # check != 0 means this flag is there
+        # check == 0 means this flag is not there
+        clean_cat = table[np.where(check == 0)[0]]
+        logger.info(f"found {len(clean_cat)} columns without this flag \n")
+        return clean_cat
