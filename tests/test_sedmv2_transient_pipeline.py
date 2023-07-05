25f8d078bae32999359a95f15431c68ff01c5e37
saarahha <saarah@sas.upenn.edu>
2023-07-05 13:49:04 -0500

    sedmv2 test: transient

---
+++ tests/test_sedmv2_transient_pipeline.py
@@ -0,0 +1,112 @@
+"""
+Module to test SEDMv2 pipeline with "default_transient" configuration
+"""
+import logging
+
+from mirar.data import Dataset, ImageBatch
+from mirar.downloader.get_test_data import get_test_data_dir
+from mirar.pipelines.sedmv2.blocks import process_transient
+from mirar.pipelines.sedmv2.load_sedmv2_image import load_raw_sedmv2_image
+from mirar.pipelines.sedmv2.sedmv2_pipeline import SEDMv2Pipeline
+from mirar.processors.utils import ImageLoader, MultiExtParser
+from mirar.testing import BaseTestCase
+
+logger = logging.getLogger(__name__)
+
+test_data_dir = get_test_data_dir()
+
+expected_zp = {
+    "ZP_4.0": 25.369777526872806,
+    "ZP_4.0_std": 0.15461781708145925,
+    "ZP_4.0_nstars": 44,
+    "ZP_6.0": 26.170285429971866,
+    "ZP_6.0_std": 0.13190330937620584,
+    "ZP_6.0_nstars": 44,
+    "ZP_8.0": 26.67797980440313,
+    "ZP_8.0_std": 0.10765968427604075,
+    "ZP_8.0_nstars": 44,
+    "ZP_10.0": 27.017411718819357,
+    "ZP_10.0_std": 0.08541367017112357,
+    "ZP_10.0_nstars": 44,
+    "ZP_AUTO": 27.501692101349697,
+    "ZP_AUTO_std": 0.05994188327036158,
+    "ZP_AUTO_nstars": 37,
+}
+
+test_configuration = [
+    MultiExtParser(
+        input_img_dir=test_data_dir,
+        input_sub_dir="raw/mef/",
+        output_img_dir=test_data_dir,
+        output_sub_dir="raw/",
+        skip_first=True,
+    ),
+    ImageLoader(
+        input_img_dir=test_data_dir,
+        input_sub_dir="raw",
+        load_image=load_raw_sedmv2_image,
+    ),
+] + process_transient
+
+pipeline = SEDMv2Pipeline(night="20230526", selected_configurations="test_transient")
+pipeline.add_configuration(
+    configuration_name="test_transient", configuration=test_configuration
+)
+
+
+class TestSEDMv2TransientPipeline(BaseTestCase):
+    """
+    Class to test SEDMv2 transient pipeline
+    """
+
+    def setUp(self):
+        """
+        Test set up
+        Returns:
+
+        """
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
+
+    def test_pipeline(self):
+        """
+        function for testing SEDMv2 transient pipeline
+        Returns:
+
+        """
+        self.logger.info("\n\n Testing SEDMv2 transient pipeline \n\n")
+
+        res, _ = pipeline.reduce_images(Dataset([ImageBatch()]), catch_all_errors=False)
+
+        self.assertEqual(len(res), 1)
+
+        header = res[0][0].get_header()
+
+        for key, value in expected_zp.items():
+            if isinstance(value, float):
+                self.assertAlmostEqual(value, header[key], places=2)
+            elif isinstance(value, int):
+                self.assertEqual(value, header[key])
+            else:
+                raise TypeError(
+                    f"Type for value ({type(value)} is neither float not int."
+                )
+
+
+if __name__ == "__main__":
+    print("Calculating latest ZP dictionary")
+
+    # Code to generate updated ZP dict of the results change
+
+    new_res, new_errorstack = pipeline.reduce_images(
+        Dataset(ImageBatch()), catch_all_errors=False
+    )
+
+    new_header = new_res[0][0].get_header()
+
+    new_exp = "expected_zp = { \n"  # pylint: disable=C0103
+    for header_key in new_header.keys():
+        if "ZP_" in header_key:
+            new_exp += f'    "{header_key}": {new_header[header_key]}, \n'
+    new_exp += "}"
+    print(new_exp)
