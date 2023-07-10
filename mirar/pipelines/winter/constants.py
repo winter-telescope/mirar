5e9b8cd4281b329325adb4613676cba0558a365c
viraj21197 <karambelkarvraj21197@gmail.com>
2023-07-10 14:38:54 +0200

    add winter stuff

--- mirar/pipelines/winter/constants.py
+++ mirar/pipelines/winter/constants.py
@@ -1,1 +1,14 @@
+"""
+Constants for the winter pipeline.
+"""
 winter_filters_map = {"Y": 1, "J": 2, "Hs": 3, "dark": 4}
+
+itid_dict = {
+    "SCIENCE": 1,
+    "BIAS": 2,
+    "FLAT": 2,
+    "DARK": 2,
+    "FOCUS": 3,
+    "POINTING": 4,
+    "OTHER": 5,
+}
