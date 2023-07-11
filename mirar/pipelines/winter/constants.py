096a1a1a51c2b91bbd73bfc7d5c5f7ffbadd2a95
viraj21197 <karambelkarvraj21197@gmail.com>
2023-07-11 07:39:31 +0200

    imgtypes

--- mirar/pipelines/winter/constants.py
+++ mirar/pipelines/winter/constants.py
@@ -3,12 +3,12 @@ Constants for the winter pipeline.
 """
 winter_filters_map = {"Y": 1, "J": 2, "Hs": 3, "dark": 4}
 
-itid_dict = {
-    "SCIENCE": 1,
-    "BIAS": 2,
-    "FLAT": 2,
-    "DARK": 2,
-    "FOCUS": 3,
-    "POINTING": 4,
-    "OTHER": 5,
+imgtype_dict = {
+    "SCIENCE": "SCIENCE",
+    "BIAS": "CAL",
+    "FLAT": "CAL",
+    "DARK": "CAL",
+    "FOCUS": "FOCUS",
+    "POINTING": "POINTING",
+    "OTHER": "NULL",
 }
