164bac3438394d84408284b47e4d070e4d46365f
viraj21197 <karambelkarvraj21197@gmail.com>
2023-07-13 04:22:30 +0200

    renive functions

--- mirar/pipelines/winter/load_winter_image.py
+++ mirar/pipelines/winter/load_winter_image.py
@@ -28,23 +28,6 @@ from mirar.pipelines.winter.models import DEFAULT_FIELD, default_program, itid_d
 logger = logging.getLogger(__name__)
 
 
-def mask_datasec(data: np.ndarray, header: astropy.io.fits.Header) -> np.ndarray:
-    """
-    Function to mask the data section of an image
-    """
-    datasec = header["DATASEC"].replace("[", "").replace("]", "").split(",")
-    datasec_xmin = int(datasec[0].split(":")[0])
-    datasec_xmax = int(datasec[0].split(":")[1])
-    datasec_ymin = int(datasec[1].split(":")[0])
-    datasec_ymax = int(datasec[1].split(":")[1])
-
-    data[:, :datasec_xmin] = np.nan
-    data[:, datasec_xmax:] = np.nan
-    data[:datasec_ymin, :] = np.nan
-    data[datasec_ymax:, :] = np.nan
-    return data
-
-
 def clean_header(header: astropy.io.fits.Header) -> astropy.io.fits.Header:
     """
     Function to clean the header of an image, adding in missing keys and
@@ -135,85 +118,6 @@ def clean_header(header: astropy.io.fits.Header) -> astropy.io.fits.Header:
     # TODO: Get puid from PROGID
     header["PUID"] = header["PROGID"]
     return header
-
-
-def load_raw_winter_image(path: str | Path) -> tuple[np.array, astropy.io.fits.Header]:
-    """
-    Function to load a raw WIRC image
-
-    :param path: path of file
-    :return: data and header of image
-    """
-    logger.debug(f"Loading {path}")
-    with fits.open(path) as img:
-        # pylint: disable=E1101
-        data = img[0].data  # pylint: disable=no-member
-        header = img[0].header
-
-        header = clean_header(header)
-
-        header[BASE_NAME_KEY] = os.path.basename(path)
-        if RAW_IMG_KEY not in header.keys():
-            header[RAW_IMG_KEY] = path
-
-        data = data.astype(float)
-
-        if "DATASEC" in header.keys():
-            data = mask_datasec(data, header)
-            del header["DATASEC"]
-        # TODO: check if this is necessary for short exposures
-        #  (Non-linearity/1700 counts)
-        data[data > 40000] = np.nan
-        if header["BOARD_ID"] == 0:
-            # data[:500, 700:1500] = np.nan
-            data[1075:, :] = np.nan
-            data[:, 1950:] = np.nan
-            data[:20, :] = np.nan
-
-        if header["BOARD_ID"] == 1:
-            pass
-
-        if header["BOARD_ID"] == 2:
-            data[1060:, :] = np.nan
-            data[:, 1970:] = np.nan
-            data[:55, :] = np.nan
-            data[:, :20] = np.nan
-
-        if header["BOARD_ID"] == 3:
-            data[1085:, :] = np.nan
-            data[:, 1970:] = np.nan
-            data[:55, :] = np.nan
-            data[:, :20] = np.nan
-
-        if header["BOARD_ID"] == 4:
-            # data[610:, :280] = np.nan
-            data[:, 1948:] = np.nan
-            data[:, :61] = np.nan
-            data[:20, :] = np.nan
-            data[1060:, :] = np.nan
-            data[:, 999:1002] = np.nan
-
-        if header["BOARD_ID"] == 5:
-            # data[740:, 1270: 1850] = np.nan
-            data[1072:, :] = np.nan
-            data[:, 1940:] = np.nan
-            data[:15, :] = np.nan
-            data[680:, 1180:] = np.nan
-            # data[data > 25000] = np.nan
-
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore", AstropyWarning)
-            _, med, std = sigma_clipped_stats(data, sigma=3.0, maxiters=5)
-
-        header["MEDCOUNT"] = med
-        header["STDDEV"] = std
-        # if header['RADEG']>300:
-        #     header['TARGNAME'] = 'other'
-
-        if "weight" in path:
-            header["OBSTYPE"] = "weight"
-
-    return data, header
 
 
 def load_proc_winter_image(path: str | Path) -> tuple[np.array, astropy.io.fits.Header]:
