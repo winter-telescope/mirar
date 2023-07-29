90fb9b66973a880eb306a5b510676c7660b5fa88
Robert Stein <rdstein@caltech.edu>
2023-07-29 15:23:51 -0700

    Avro and IPAC (#566)

--- mirar/processors/alerts/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Module for producing, and broadcasting, avro alerts
-"""
-from mirar.processors.alerts.avro_alert import AvroPacketMaker
-from mirar.processors.alerts.send_to_fritz import SendToFritz
