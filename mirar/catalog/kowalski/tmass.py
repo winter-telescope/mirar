30955f53659b1eea32359b269ba9e332f8679c0b
Robert Stein <rdstein@caltech.edu>
2023-08-01 13:46:22 -0700

    AB mag

--- mirar/catalog/kowalski/tmass.py
+++ mirar/catalog/kowalski/tmass.py
@@ -1,6 +1,7 @@
 """
 Module for querying 2MASS using Kowalski
 """
+from mirar.catalog.gaia import offsets_2mass
 from mirar.catalog.kowalski.base_kowalski_catalog import BaseKowalskiXMatch
 
 
@@ -50,3 +51,31 @@ class TMASS(BaseKowalskiXMatch):
         "tmobjectid": str,
         "tmph_qual": str,
     }
+
+    @staticmethod
+    def update_data(data: dict) -> dict:
+        """
+        For a given catalog, update the data with any extra information
+
+        :param data: kowalski data
+        :return: updated data
+        """
+
+        new = {}
+
+        for key, val in data.items():
+            new_list = []
+
+            for vega_dict in val:
+                ab_dict = dict(vega_dict)
+
+                for tmass_filter, offset in offsets_2mass.items():
+                    tmass_key = f"{tmass_filter}_m"
+                    vega_mag = ab_dict[tmass_key]
+                    ab_dict[tmass_key] = vega_mag + offset
+
+                new_list.append(ab_dict)
+
+            new[key] = new_list
+
+        return data
