ba96a7267cba2eaa76f58ef51d9a965a36fd5c16
viraj21197 <karambelkarvraj21197@gmail.com>
2023-08-09 09:39:22 -0700

    remove source filterer

--- mirar/processors/sources/source_filter.py
+++ mirar/processors/sources/source_filter.py
@@ -2,9 +2,6 @@
 Base class for source filters
 """
 import logging
-from typing import Callable
-
-import pandas as pd
 
 from mirar.data import SourceBatch
 from mirar.processors.base_processor import BaseSourceProcessor
@@ -21,25 +18,3 @@ class BaseSourceFilter(BaseSourceProcessor):
 
     def _apply_to_sources(self, batch: SourceBatch) -> SourceBatch:
         raise NotImplementedError
-
-
-class SourceFilterwithFunction(BaseSourceFilter):
-    """
-    Class to filter sources based on a function
-    """
-
-    base_key = "filter_using_function"
-
-    def __init__(self, filter_function: Callable[[pd.DataFrame], pd.DataFrame]):
-        super().__init__()
-        self.filter_function = filter_function
-
-    def __str__(self) -> str:
-        return "Processor to filter sources based on a function."
-
-    def _apply_to_sources(self, batch: SourceBatch) -> SourceBatch:
-        for source_list in batch:
-            candidate_table = source_list.get_data()
-            filtered_table = self.filter_function(candidate_table)
-            source_list.set_data(filtered_table)
-        return batch
