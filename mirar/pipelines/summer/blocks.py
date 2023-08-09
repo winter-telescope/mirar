6b70bbbb12bf8016f9ba628fbdf947a9d3fc78ba
viraj21197 <karambelkarvraj21197@gmail.com>
2023-08-09 10:22:05 -0700

    update to match

--- mirar/pipelines/summer/blocks.py
+++ mirar/pipelines/summer/blocks.py
@@ -46,7 +46,7 @@ from mirar.processors.photometry.aperture_photometry import CandidateAperturePho
 from mirar.processors.photometry.psf_photometry import CandidatePSFPhotometry
 from mirar.processors.reference import ProcessReference
 from mirar.processors.sources import SourceWriter
-from mirar.processors.sources.source_detector import SourceDetector
+from mirar.processors.sources.source_detector import ZOGYSourceDetector
 from mirar.processors.sources.utils import RegionsWriter
 from mirar.processors.utils import ImageBatcher, ImageLoader, ImageSaver, ImageSelector
 from mirar.processors.utils.cal_hunter import CalHunter
@@ -227,7 +227,7 @@ export_diff_to_db = [
 ]
 
 extract_candidates = [
-    SourceDetector(output_sub_dir="subtract", **sextractor_candidates_config),
+    ZOGYSourceDetector(output_sub_dir="subtract", **sextractor_candidates_config),
     RegionsWriter(output_dir_name="candidates"),
     CandidatePSFPhotometry(),
     CandidateAperturePhotometry(
