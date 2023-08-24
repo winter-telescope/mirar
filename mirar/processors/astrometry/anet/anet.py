53f05e95445a91928b048c2264a46c7e5b3c304e
viraj21197 <karambelkarvraj21197@gmail.com>
2023-08-24 14:52:04 -0700

    remove default sextractor from anet

--- mirar/processors/astrometry/anet/anet.py
+++ mirar/processors/astrometry/anet/anet.py
@@ -72,7 +72,6 @@ def run_astrometry_net_single(
         f"--new-fits {newname} "
         f"--overwrite "
         f"--out {basename} "  # use this base name for outputs (instead of 'temp_...')
-        f"--use-source-extractor "
     )
 
     if scale_bounds is not None:
