58dc353fc62bea50ea2ecaeb810ef23f5346e83e
jamiesoon <jamiesoon@users.noreply.github.com>
2023-08-25 16:33:46 +1000

    remove space.

--- mirar/database/engine.py
+++ mirar/database/engine.py
@@ -25,8 +25,7 @@ def get_engine(
     """
 
     return create_engine(
-        f"postgresql+psycopg://{db_user}:{db_password}"
-        f"@{db_hostname}:{db_port}/{db_name}",
+        f"postgresql+psycopg://{db_user}:{db_password}@/{db_name}",
         future=True,
         poolclass=NullPool,
     )
