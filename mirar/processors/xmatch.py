00ffa5b439d0cae6cfbc40a6f25f9dfc4f62ad02
viraj21197 <karambelkarvraj21197@gmail.com>
2024-01-21 18:33:40 -0800

    calculate distance field for source xmatch

--- mirar/processors/xmatch.py
+++ mirar/processors/xmatch.py
@@ -3,7 +3,9 @@ Module to cross-match a candidate_table with different catalogs
 """
 import logging
 
+import astropy.units as u
 import numpy as np
+from astropy.coordinates import SkyCoord
 
 from mirar.catalog.base_catalog import BaseXMatchCatalog
 from mirar.data import SourceBatch
@@ -35,6 +37,7 @@ class XMatch(BaseSourceProcessor):
 
             ras = candidate_table["ra"]
             decs = candidate_table["dec"]
+            crds = SkyCoord(ras, decs, unit=u.deg)
             query_names = np.array([f"q{x}" for x in np.arange(len(ras))])
 
             catalog = self.catalog
@@ -49,6 +52,7 @@ class XMatch(BaseSourceProcessor):
                 if catalog.projection[k] == 1:
                     available_projection_keys += [k]
 
+            # Add placeholder columns for each catalog column
             for key in available_projection_keys:
                 for num in range(self.catalog.num_sources):
                     colname = catalog.column_names[key]
@@ -56,8 +60,11 @@ class XMatch(BaseSourceProcessor):
                         np.nan,
                         dtype=catalog.column_dtypes[colname],
                     )
+
+            # Add column for number of matches
             nmatch_colname = f"nmtch{self.catalog.abbreviation}"
             candidate_table[nmatch_colname] = 0
+
             for query_ind, query_name in enumerate(query_names):
                 results = query_results[query_name]
                 for result_ind, result in enumerate(results):
@@ -66,6 +73,22 @@ class XMatch(BaseSourceProcessor):
                         candidate_table.at[query_ind, colname] = result[key]
 
                 candidate_table.at[query_ind, nmatch_colname] = len(results)
+
+            # Calculate distances between query and result and add to table
+            for num in range(self.catalog.num_sources):
+                result_ra_colname = self.catalog.ra_column_name + f"{num + 1}"
+                result_dec_colname = self.catalog.dec_column_name + f"{num + 1}"
+                dist_colname = f"dist{self.catalog.abbreviation}nr{num + 1}"
+                candidate_table[dist_colname] = np.array(np.nan, dtype=float)
+                crd_nanmask = np.invert(np.isnan(candidate_table[result_ra_colname]))
+                result_crds = SkyCoord(
+                    ra=candidate_table[result_ra_colname][crd_nanmask],
+                    dec=candidate_table[result_dec_colname][crd_nanmask],
+                    unit=u.deg,
+                )
+                candidate_table[dist_colname][crd_nanmask] = (
+                    crds[crd_nanmask].separation(result_crds).arcsec
+                )
 
             candidate_table = candidate_table.replace({np.nan: None})
 
