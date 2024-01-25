5ed0974a36352314749dc0172284536657e68c3c
viraj21197 <karambelkarvraj21197@gmail.com>
2024-01-25 13:39:12 -0800

    fix

--- mirar/processors/photcal/zp_calculator.py
+++ mirar/processors/photcal/zp_calculator.py
@@ -140,7 +140,7 @@ class ZPWithColorTermCalculator(BaseZeroPointCalculator):
 
     def __init__(
         self,
-        color_colnames_generator: Callable[[Image], [str, str]],
+        color_colnames_generator: Callable[[Image], list[str, str]],
     ):
         self.color_colnames_generator = color_colnames_generator
 
