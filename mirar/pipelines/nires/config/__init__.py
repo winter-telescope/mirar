fd9f46b4b945a966bb1ec93fcc4d6c432e462cfe
viraj21197 <karambelkarvraj21197@gmail.com>
2024-02-27 19:55:13 -0800

    cleanup

--- mirar/pipelines/nires/config/__init__.py
+++ mirar/pipelines/nires/config/__init__.py
@@ -10,73 +10,11 @@ from mirar.processors.utils.cal_hunter import CalRequirement
 
 PIPELINE_NAME = "nires"
 
-winter_file_dir = Path(__file__).parent.joinpath("files")
+file_dir = Path(__file__).parent.joinpath("files")
 
 sextractor_astrometry_config = {
-    "config_path": winter_file_dir.joinpath("astrom.sex"),
-    "filter_path": winter_file_dir.joinpath("default.conv"),
-    "parameter_path": winter_file_dir.joinpath("astrom.param"),
-    "starnnw_path": winter_file_dir.joinpath("default.nnw"),
+    "config_path": file_dir.joinpath("astrom.sex"),
+    "filter_path": file_dir.joinpath("default.conv"),
+    "parameter_path": file_dir.joinpath("astrom.param"),
+    "starnnw_path": file_dir.joinpath("default.nnw"),
 }
-
-sextractor_astromstats_config = sextractor_astrometry_config
-sextractor_astromstats_config["parameter_path"] = winter_file_dir.joinpath(
-    "astromstats.param"
-)
-
-sextractor_photometry_config = {
-    "config_path": winter_file_dir.joinpath("photomCat.sex"),
-    "filter_path": winter_file_dir.joinpath("default.conv"),
-    "parameter_path": winter_file_dir.joinpath("photom.param"),
-    "starnnw_path": winter_file_dir.joinpath("default.nnw"),
-}
-
-sextractor_photometry_psf_config = {
-    "config_path": winter_file_dir.joinpath("photomCat.sex"),
-    "filter_path": winter_file_dir.joinpath("default.conv"),
-    "parameter_path": winter_file_dir.joinpath("photom_psf.param"),
-    "starnnw_path": winter_file_dir.joinpath("default.nnw"),
-}
-
-
-sextractor_anet_config = {
-    "config_path_boardid_1_5": winter_file_dir.joinpath("astrom_anet_1_5.sex"),
-    "config_path_boardid_0_2_3_4": winter_file_dir.joinpath("astrom_anet_0_2_3_4.sex"),
-    "filter_path": winter_file_dir.joinpath("default.conv"),
-    "parameter_path": winter_file_dir.joinpath("astrom.param"),
-    "starnnw_path": winter_file_dir.joinpath("default.nnw"),
-}
-
-sextractor_reference_config = {
-    "config_path": winter_file_dir.joinpath("reference.sex"),
-    "parameter_path": winter_file_dir.joinpath("photom.param"),
-    "filter_path": winter_file_dir.joinpath("default.conv"),
-    "starnnw_path": winter_file_dir.joinpath("default.nnw"),
-}
-
-sextractor_reference_psf_phot_config = {
-    "config_path": winter_file_dir.joinpath("reference.sex"),
-    "parameter_path": winter_file_dir.joinpath("photom_psf.param"),
-    "filter_path": winter_file_dir.joinpath("default.conv"),
-    "starnnw_path": winter_file_dir.joinpath("default.nnw"),
-}
-
-sextractor_candidate_config = {
-    "cand_det_sextractor_config": winter_file_dir.joinpath("candidate_detection.sex"),
-    "cand_det_sextractor_nnw": winter_file_dir.joinpath("default.nnw"),
-    "cand_det_sextractor_filter": winter_file_dir.joinpath("default.conv"),
-    "cand_det_sextractor_params": winter_file_dir.joinpath("Scorr.param"),
-}
-
-swarp_config_path = winter_file_dir.joinpath("config.swarp")
-scamp_config_path = winter_file_dir.joinpath("astrom.scamp")
-winter_mask_path = winter_file_dir.joinpath("winter_mask.fits")
-
-psfex_path = winter_file_dir.joinpath("photom.psfex")
-ref_psfex_path = winter_file_dir.joinpath("reference.psfex")
-
-winter_cal_requirements = [
-    CalRequirement(
-        target_name="dark", required_field="EXPTIME", required_values=["120.0"]
-    ),
-]
--- mirar/pipelines/nires/generator.py
+++ mirar/pipelines/nires/generator.py
@@ -1,12 +1,4 @@
 from mirar.catalog.vizier.ukidssgps import UkidssGPS
-from mirar.pipelines.nires.config import sextractor_anet_config
-
-
-def nires_sextractor_config_path_generator(_) -> str:
-    """
-    Generates the sextractor config file path for the NIRES image
-    """
-    return sextractor_anet_config["config_path_boardid_0_2_3_4"]
 
 
 def nires_astrometric_ref_catalog_generator(_) -> UkidssGPS:
