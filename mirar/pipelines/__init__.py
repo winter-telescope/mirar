12cbbd393e63d2b7fbb186d395c2c7c8e5ff1083
jamiesoon <jamiesoon@users.noreply.github.com>
2024-04-30 11:47:44 +1000

    Use importlib instead of importing all

--- mirar/pipelines/__init__.py
+++ mirar/pipelines/__init__.py
@@ -2,16 +2,11 @@
 Central location for all pipelines. This is where you should add new pipelines.
 """
 
+import importlib
 import logging
 
 from mirar.errors import ProcessorError
 from mirar.pipelines.base_pipeline import Pipeline
-from mirar.pipelines.git.git_pipeline import GITPipeline
-from mirar.pipelines.sedmv2.sedmv2_pipeline import SEDMv2Pipeline
-from mirar.pipelines.summer.summer_pipeline import SummerPipeline
-from mirar.pipelines.wasp.wasp_pipeline import WASPPipeline
-from mirar.pipelines.winter.winter_pipeline import WINTERPipeline
-from mirar.pipelines.wirc.wirc_pipeline import WircPipeline
 
 logger = logging.getLogger(__name__)
 
@@ -38,13 +33,20 @@ def get_pipeline(
     :return: pipeline
     """
     try:
+        importlib.import_module(
+            f"mirar.pipelines.{instrument.lower()}.{instrument.lower()}_pipeline"
+        )
         pipeline = Pipeline.pipelines[instrument.lower()]
         logger.info(f"Found {instrument} pipeline")
     except KeyError as exc:
         err = (
-            f"Unrecognised pipeline {instrument}. "
+            f"Unrecognised pipeline {instrument}."
             f"Available pipelines are: {Pipeline.pipelines.keys()}"
         )
+        logger.error(err)
+        raise PipelineConfigError(err) from exc
+    except ModuleNotFoundError as exc:
+        err = f"Unrecognised pipeline {instrument}."
         logger.error(err)
         raise PipelineConfigError(err) from exc
 
