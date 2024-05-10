38cc3104fdd907f40d5de6a0c106ebe5c9332d38
Robert Stein <rdstein@caltech.edu>
2024-05-10 16:01:20 -0700

    cleanup dithergrp

--- mirar/pipelines/winter/generator/utils.py
+++ mirar/pipelines/winter/generator/utils.py
@@ -61,18 +61,15 @@ def winter_ref_catalog_namer(image: Image, output_dir: Path) -> Path:
     """
     output_dir.mkdir(exist_ok=True, parents=True)
 
-    dither_grp = ""
-    if "astrometric" in output_dir.as_posix():
-        dither_grp = image["DITHGRP"]
     if image["FIELDID"] != DEFAULT_FIELD:
         ref_cat_path = (
             output_dir / f"field{image['FIELDID']}_{image['SUBDETID']}"
-            f"_{dither_grp}_{image['FILTER']}.ldac.cat"
+            f"_{image['FILTER']}.ldac.cat"
         )
     else:
         ref_cat_path = (
             output_dir / f"field{image['FIELDID']}_{image['SUBDETID']}_"
-            f"_{dither_grp}_{image['TARGNAME']}_{image[TIME_KEY]}"
+            f"_{image['TARGNAME']}_{image[TIME_KEY]}"
             f"_{image['FILTER']}.ldac.cat"
         )
     return ref_cat_path
