45e39f9b3a750830811a8a171d607765ddd6b86f
Robert Stein <rdstein@caltech.edu>
2024-06-12 14:43:49 -0700

    Filter out galactic plane post-imsub

--- mirar/pipelines/winter/blocks.py
+++ mirar/pipelines/winter/blocks.py
@@ -665,6 +665,7 @@ imsub = [
     ImageSaver(output_dir_name="diffs"),
     DatabaseImageInserter(db_table=Diff, duplicate_protocol="replace"),
     ImageSaver(output_dir_name="subtract"),
+    ImageRejector(("PROGNAME", ["2023A007"])),  # Filter out galactic data
 ]
 
 load_sub = [
