bc2ba7e004b931c9b863232d5a1c773af5ac96cd
Robert Stein <rdstein@caltech.edu>
2024-06-17 16:33:51 -0700

    Fix typo in cal requirements

--- mirar/pipelines/winter/config/__init__.py
+++ mirar/pipelines/winter/config/__init__.py
@@ -106,7 +106,7 @@ winter_cal_requirements = [
         ],
     ),
     CalRequirement(
-        target_name="domeflat", required_field="FILTER", required_values=["Y", "J", "Hs"]
+        target_name="flat", required_field="FILTER", required_values=["Y", "J", "H"]
     ),
 ]
 
