956a43da1200b57431f2680bc9b4eae139fd761c
Robert Stein <rdstein@caltech.edu>
2024-11-27 14:36:48 -0500

    New nlc mode

--- mirar/pipelines/winter/nlc.py
+++ mirar/pipelines/winter/nlc.py
@@ -1,11 +1,16 @@
-from winternlc.non_linear_correction import nlc_single
+"""
+Module for applying WINTER non-linear correction to images
+"""
+
+from winternlc.non_linear_correction import apply_nlc_single
 
 from mirar.data import ImageBatch
 
 
 def apply_winter_nlc(images: ImageBatch) -> ImageBatch:
     """
-    Apply WINTER non-linear correction to images
+    Apply WINTER non-linear correction to images.
+    Uses header information to apply the correct correction.
 
     :param images: ImageBatch to apply non-linear correction to
     :return: Corrected ImageBatch
@@ -13,8 +18,8 @@ def apply_winter_nlc(images: ImageBatch) -> ImageBatch:
 
     for image in images:
         data = image.get_data()
-        board_id = image["BOARD_ID"]
-        corrected_image = nlc_single(data, board_id)
-        image.set_data(corrected_image)
+        header = image.get_header()
+        corrected_data = apply_nlc_single(data, header)
+        image.set_data(corrected_data)
 
     return images
