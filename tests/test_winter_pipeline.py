8fa74e9c9129c7b6e85c6c55b0bdadd66f6b9983
viraj21197 <karambelkarvraj21197@gmail.com>
2025-10-31 12:57:00 -0400

    update test

--- tests/test_winter_pipeline.py
+++ tests/test_winter_pipeline.py
@@ -147,7 +147,7 @@ class TestWinterPipeline(BaseTestCase):
 
         candidates_table = source_table.get_data()
 
-        self.assertEqual(len(candidates_table), 153)
+        self.assertEqual(len(candidates_table), 129)
         for key, value in expected_dataframe_values.items():
             if isinstance(value, list):
                 for ind, val in enumerate(value):
